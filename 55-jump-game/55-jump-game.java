6d1944193c8d595fba9208dd1e3517a44cbc3e10
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2022-08-28 20:02:30 -0400

    Time: 121 ms (27.10%), Space: 70.7 MB (5.46%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,11 +1,33 @@
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    int[] precomputeSteps;
+    
     public boolean canJump(int[] nums) {
-        int lastPos = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
+        
+        int n = nums.length;
+        precomputeSteps = new int[n];
+        
+        Arrays.fill(precomputeSteps, 0);
+        precomputeSteps[n-1] = 1;
+        
+        for(int i = n-2; i>= 0; i --){
+            int furthestIndex = Math.min(n-1, nums[i] + i);
+            for(int j = i + 1; j <= furthestIndex; j += 1){
+                if(precomputeSteps[j] == 1){
+                    precomputeSteps[i] = 1;
+                    break;
+                }
             }
+            
         }
-        return lastPos == 0;
+        
+        if(precomputeSteps[0] == 1){
+            return true;
+        }
+        
+        return false;
+        
     }
+    
 }
