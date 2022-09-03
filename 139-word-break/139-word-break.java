aa0c0cbd6ef498ef028c37885790f87b59ef15db
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2022-09-03 13:26:16 -0400

    Time: 15 ms (34.74%), Space: 47.7 MB (18.07%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,20 +1,24 @@
 public class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
-    }
-
-    private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
-        if (start == s.length()) {
-            return true;
-        }
-        if (memo[start] != null) {
-            return memo[start];
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
-                return memo[start] = true;
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start]) {
+                continue;
             }
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (wordDictSet.contains(s.substring(start, end))) {
+                    queue.add(end);
+                    if (end == s.length()) {
+                        return true;
+                    }
+                }
+            }
+            visited[start] = true;
         }
-        return memo[start] = false;
+        return false;
     }
 }
