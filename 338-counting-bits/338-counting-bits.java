d7e0c1af66e724b8192259c9a8837deac32723b8
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2022-09-06 18:59:08 -0400

    Time: 3 ms (56.43%), Space: 47.4 MB (83.97%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,20 +1,10 @@
 public class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
-        int x = 0;
-        int b = 1;
-    
-        // [0, b) is calculated
-        while (b <= n) {
-            // generate [b, 2b) or [b, n) from [0, b)
-            while (x < b && x + b <= n) {
-                ans[x + b] = ans[x] + 1;
-                ++x;
-            }                         
-            x = 0; // reset x
-            b <<= 1; // b = 2b
+        for (int x = 1; x <= n; ++x) {
+            // x / 2 is x >> 1 and x % 2 is x & 1
+            ans[x] = ans[x >> 1] + (x & 1); 
         }
-                  
         return ans;
     }
 }
