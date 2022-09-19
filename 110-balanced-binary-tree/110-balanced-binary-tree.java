a3a80d3af551b0e2d107b11710b94947a0466475
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2022-09-19 17:55:18 -0400

    Time: 2 ms (34.55%), Space: 44.4 MB (50.48%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -13,20 +13,45 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return getHeight(root) != -1;
-    }
-    
-       private int getHeight(TreeNode node) {
-        if (node == null) return 0;
+// Utility class to store information from recursive calls
+final class TreeInfo {
+  public final int height;
+  public final boolean balanced;
 
-        int left = getHeight(node.left);
-        int right = getHeight(node.right);
-
-        // left, right subtree is unbalanced or cur tree is unbalanced
-        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
-
-        return Math.max(left, right) + 1;
-    }
+  public TreeInfo(int height, boolean balanced) {
+    this.height = height;
+    this.balanced = balanced;
+  }
 }
+
+class Solution {
+  // Return whether or not the tree at root is balanced while also storing
+  // the tree's height in a reference variable.
+  private TreeInfo isBalancedTreeHelper(TreeNode root) {
+    // An empty tree is balanced and has height = -1
+    if (root == null) {
+      return new TreeInfo(-1, true);
+    }
+
+    // Check subtrees to see if they are balanced.
+    TreeInfo left = isBalancedTreeHelper(root.left);
+    if (!left.balanced) {
+      return new TreeInfo(-1, false);
+    }
+    TreeInfo right = isBalancedTreeHelper(root.right);
+    if (!right.balanced) {
+      return new TreeInfo(-1, false);
+    }
+
+    // Use the height obtained from the recursive calls to
+    // determine if the current node is also balanced.
+    if (Math.abs(left.height - right.height) < 2) {
+      return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+    }
+    return new TreeInfo(-1, false);
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    return isBalancedTreeHelper(root).balanced;
+  }
+};
