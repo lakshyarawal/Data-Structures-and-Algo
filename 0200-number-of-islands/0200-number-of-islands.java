44b33972f13754c254ea06ea619bc1f1119d4248
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2022-10-11 23:19:39 -0400

    Time: 6 ms (49.63%), Space: 49.7 MB (99.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,45 @@
 class Solution {
-   public static void explore(char[][] grid, int i, int j) {
-		grid[i][j]='x';
-		for(int d=0;d<dx.length;d++) {
-			if(i+dy[d]<grid.length && i+dy[d]>=0 && j+dx[d]<grid[0].length && j+dx[d]>=0 && grid[i+dy[d]][j+dx[d]]=='1') {
-				explore(grid,i+dy[d],j+dx[d]);
-			}
-		}
-	}
-    static int[] dx = {-1,0,0,1};
-    static int[] dy = {0,1,-1,0};
-        
-    public static int numIslands(char[][] grid) {
-        if(grid==null || grid.length==0) return 0;
-        int islands = 0;
-        for(int i=0;i<grid.length;i++) {
-            for(int j=0;j<grid[i].length;j++) {
-                if(grid[i][j]=='1') {
-                    explore(grid,i,j);
-                    islands++;
-                }
-            }
-        }
-        return islands;
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
     }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          grid[r][c] = '0'; // mark as visited
+          Queue<Integer> neighbors = new LinkedList<>();
+          neighbors.add(r * nc + c);
+          while (!neighbors.isEmpty()) {
+            int id = neighbors.remove();
+            int row = id / nc;
+            int col = id % nc;
+            if (row - 1 >= 0 && grid[row-1][col] == '1') {
+              neighbors.add((row-1) * nc + col);
+              grid[row-1][col] = '0';
+            }
+            if (row + 1 < nr && grid[row+1][col] == '1') {
+              neighbors.add((row+1) * nc + col);
+              grid[row+1][col] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col-1] == '1') {
+              neighbors.add(row * nc + col-1);
+              grid[row][col-1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col+1] == '1') {
+              neighbors.add(row * nc + col+1);
+              grid[row][col+1] = '0';
+            }
+          }
+        }
+      }
+    }
+
+    return num_islands;
+  }
 }
