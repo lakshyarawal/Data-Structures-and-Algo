3be8bf4299459e3a3ac58c39ff1e26849b4b5bd6
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-01-05 14:10:05 -0500

    Time: 137 ms (41.48%), Space: 57.3 MB (30.94%) - LeetHub

---
+++ 0975-odd-even-jump/0975-odd-even-jump.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int oddEvenJumps(int[] arr) {
+        int n  = arr.length, res = 1;
+        boolean[] higher = new boolean[n], lower = new boolean[n];
+        higher[n - 1] = lower[n - 1] = true;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(arr[n - 1], n - 1);
+        for (int i = n - 2; i >= 0; --i) {
+            Map.Entry<Integer, Integer> hi = map.ceilingEntry(arr[i]), lo = map.floorEntry(arr[i]);
+            if (hi != null) higher[i] = lower[(int)hi.getValue()];
+            if (lo != null) lower[i] = higher[(int)lo.getValue()];
+            if (higher[i]) res++;
+            map.put(arr[i], i);
+        }
+        return res;
+    }
+}
