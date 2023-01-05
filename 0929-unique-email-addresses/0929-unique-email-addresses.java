2c9aab9f9ae28cd53ecc96e8ca4c78e1c1e3b9ae
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-01-05 17:44:57 -0500

    Time: 35 ms (40.55%), Space: 51.1 MB (26.39%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,37 +1,19 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-         HashSet<String> hs = new HashSet();  
-        
-        for(int i = 0; i< emails.length; i++){
-            String curr = emails[i];
-            StringBuilder sb = new StringBuilder();
-            boolean domain = false;
-            boolean isValid = true;
-            for(int j = 0; j<curr.length(); j++){
-                
-                char a = curr.charAt(j);
-                
-                if(!domain){
-                    if(a == '+'){
-                        while(j < curr.length() && curr.charAt(j) != '@') j++;
-                        sb.append("@");
-                        domain = true;
-                    }
-                    else if(a == '.') continue;
-                    else{
-                        if(a == '@') domain = true;
-                        sb.append(Character.toString(a));
-                    }
-                }
-                else{
-                    if(a == '@') isValid = false;
-                    sb.append(Character.toString(a));
-                }
-            }
-            System.out.println(sb.toString());
-            if(isValid) hs.add(sb.toString());
+        // hash set to store all the unique emails
+        Set<String> uniqueEmails = new HashSet<>();
+
+        for (String email : emails) {
+            // split into two parts local and domain
+            String[] parts = email.split("@");
+
+            // split local by '+'
+            String[] local = parts[0].split("\\+");
+
+            // remove all '.', and concatenate '@' and append domain
+            uniqueEmails.add(local[0].replace(".", "") + "@" + parts[1]);
         }
-             
-        return hs.size();
+
+        return uniqueEmails.size();
     }
 }
