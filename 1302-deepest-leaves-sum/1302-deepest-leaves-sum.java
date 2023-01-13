2b5e5befcad6305b3a51196e3a314225bf680737
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-01-13 17:15:51 -0500

    Time: 5 ms (61.62%), Space: 44.3 MB (92.50%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,28 +15,26 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        int sum = 0;
-        int max_level = 0;
-        LinkedList<TreeNode> dfs = new LinkedList<>();
-        HashMap<TreeNode, Integer> hm = new HashMap<>();
-        hm.put(root, 0);
-        dfs.add(root);
-        while(!dfs.isEmpty()){
-            TreeNode curr_node = dfs.remove(0);
-            int node_level = hm.get(curr_node);
-            //System.out.println("Current Node: "+ curr_node.val+" Level: "+ node_level);
-            
-            if(node_level > max_level){sum = curr_node.val; max_level = node_level;}
-            else if(node_level == max_level) sum += curr_node.val;
-            if(curr_node.left != null) {
-                dfs.add(curr_node.left); 
-                hm.put(curr_node.left, node_level+1);
-                };
-            if(curr_node.right != null){
-                dfs.add(curr_node.right);
-                hm.put(curr_node.right, node_level+1);
+        if(root == null)
+            return 0;
+        
+        int prev = 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+
+        while(q.isEmpty() == false){
+            int cnt = q.size();
+            prev = 0;
+            for(int i=0;i<cnt;i++){
+                TreeNode curr = q.poll();
+                prev += curr.val;
+                if(curr.left!=null)
+                    q.add(curr.left);
+                if(curr.right!=null)
+                    q.add(curr.right);
             }
         }
-        return sum;
+
+        return prev;
     }
 }
