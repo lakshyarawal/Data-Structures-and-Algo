ef8c429b93032afe233131f93e2e5573b7023632
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-01-18 22:47:50 -0500

    Time: 7 ms (88.09%), Space: 43.6 MB (55.26%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,37 +4,27 @@ class Solution {
         if(p.length() > s.length()) return indices;
         
         int[] char_count = new int[26];
+        
         for(int i = 0; i < p.length(); i++){
             char_count[p.charAt(i) - 'a']++;
         }
         
         int[] temp = new int[26];
+        
         for(int j = 0; j < s.length(); j++){
             temp[s.charAt(j) - 'a']++;
-            if(j == p.length()-1){
-                Boolean isPos = true;
-                //System.out.println(Arrays.toString(temp));
-                //System.out.println(Arrays.toString(char_count));
-                for(int i = 0; i < temp.length; i++){
-                    if(temp[i] != char_count[i]) isPos = false;
-                }
-                if(isPos)indices.add(j-p.length()+1);
-            }
-            if(j >= p.length()){
+            Boolean isPos = true;
+            if(j > p.length()-1){
                 temp[s.charAt(j-p.length()) - 'a']--;
-                Boolean isPos = true;
-                //System.out.println("Index: "+ j);
-                //System.out.println(Arrays.toString(temp));
-                //System.out.println(Arrays.toString(char_count));
+            }
+            if(j > p.length() - 2){
                 for(int i = 0; i < temp.length; i++){
                     if(temp[i] != char_count[i]) isPos = false;
                 }
                 if(isPos)indices.add(j-p.length()+1);
             }
-            
-            //System.out.println("Sum for index: "+  Arrays.stream(temp).sum());
+
         }
-        
         return indices;
     }
 }
