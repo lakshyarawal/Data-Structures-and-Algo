ea8cdd0da9f3944f66379158ca651927e41b8909
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-02-14 14:16:15 -0500

    Time: 3 ms (32.17%), Space: 44.1 MB (77.09%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,25 +1,41 @@
 class Solution {
-    
-    private List<List<Integer>> paths;
-    
-    public void explorePaths(int source, int target, List<Integer> curPath, int[][] graph){
-        for(int i = 0; i< graph[source].length; i++){
-            List<Integer> n = new ArrayList<>(curPath); 
-            n.add(graph[source][i]);
-            if(graph[source][i] == target){
-                paths.add(n);
-            }
-            else{
-               explorePaths(graph[source][i], target, n, graph);
+    private int target;
+    private int[][] graph;
+    private HashMap<Integer, List<List<Integer>>> memo;
+
+    protected List<List<Integer>> allPathsToTarget(int currNode) {
+        // memoization. check the result in the cache first
+        if (memo.containsKey(currNode))
+            return memo.get(currNode);
+
+        List<List<Integer>> results = new ArrayList<>();
+        // base case
+        if (currNode == this.target) {
+            ArrayList<Integer> path = new ArrayList<>();
+            path.add(target);
+            results.add(path);
+            return results;
+        }
+
+        // iterate through the paths starting from each neighbor.
+        for (int nextNode : this.graph[currNode]) {
+            for (List<Integer> path : allPathsToTarget(nextNode)) {
+                ArrayList<Integer> newPath = new ArrayList<>();
+                newPath.add(currNode);
+                newPath.addAll(path);
+                results.add(newPath);
             }
         }
+        memo.put(currNode, results);
+        return results;
     }
-        
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        paths = new ArrayList<>();
-        List<Integer> c = new ArrayList<>();
-        c.add(0);
-        explorePaths(0, graph.length-1, c, graph);
-        return paths;
+
+        this.target = graph.length - 1;
+        this.graph = graph;
+        this.memo = new HashMap<>();
+
+        return this.allPathsToTarget(0);
     }
 }
