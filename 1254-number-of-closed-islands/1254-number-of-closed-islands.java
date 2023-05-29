e8ef8a42d389da6a6489b70a272aabfea571123e
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-05-29 19:22:50 -0400

    Time: 1 ms (100.00%), Space: 43.9 MB (5.06%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,19 +1,50 @@
 class Solution {
-   int fill(int[][] g, int i, int j) {
-  if (i < 0 || j < 0 || i >= g.length || j >= g[i].length || g[i][j] != 0)
-    return 0;
-  return (g[i][j] = 1) + fill(g, i + 1, j) + fill(g, i, j + 1)
-    + fill(g, i - 1, j) + fill(g, i, j - 1);
-}
-public int closedIsland(int[][] g) {
-  for (int i = 0; i < g.length; ++i)
-    for (int j = 0; j < g[i].length; ++j)
-      if (i * j * (i - g.length + 1) * (j - g[i].length + 1) == 0)
-        fill(g, i, j);
-  int res = 0;
-  for (int i = 0; i < g.length; ++i)
-    for (int j = 0; j < g[i].length; ++j)
-      res += fill(g, i, j) > 0 ? 1 : 0;
-  return res;
-}
+    private void markLand(int[][] grid, int i, int j){
+        grid[i][j] = 1;
+        
+        if(i>0 && grid[i-1][j] == 0)
+            markLand(grid, i-1, j);
+        
+        if(j>0 && grid[i][j-1] == 0)
+            markLand(grid, i, j-1);
+        
+        if(i<grid.length-1 && grid[i+1][j] == 0)
+            markLand(grid, i+1, j);
+        
+        if(j<grid[0].length-1 && grid[i][j+1] == 0)
+            markLand(grid, i, j+1);
+    }
+    public int closedIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i=0; i<m; i++){
+            if(grid[i][0] == 0)
+                markLand(grid, i, 0);
+            
+            if(grid[i][n-1] == 0)
+                markLand(grid, i, n-1);
+        }
+        
+        for(int i=0; i<n; i++){
+            if(grid[0][i] == 0)
+                markLand(grid, 0, i);
+            
+            if(grid[m-1][i] == 0)
+                markLand(grid, m-1, i);
+        }
+        
+        
+        int closedIslands = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 0){
+                    closedIslands++;
+                    markLand(grid, i, j);
+                }
+            }
+        }
+        
+        return closedIslands;
+    }
 }
