784342ae32b0148d548f61f41e9d94606c4f68b6
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-05-29 19:27:17 -0400

    Time: 2 ms (85.15%), Space: 43.4 MB (24.11%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,50 +1,30 @@
 class Solution {
-    private void markLand(int[][] grid, int i, int j){
-        grid[i][j] = 1;
-        
-        if(i>0 && grid[i-1][j] == 0)
-            markLand(grid, i-1, j);
-        
-        if(j>0 && grid[i][j-1] == 0)
-            markLand(grid, i, j-1);
-        
-        if(i<grid.length-1 && grid[i+1][j] == 0)
-            markLand(grid, i+1, j);
-        
-        if(j<grid[0].length-1 && grid[i][j+1] == 0)
-            markLand(grid, i, j+1);
-    }
     public int closedIsland(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        for(int i=0; i<m; i++){
-            if(grid[i][0] == 0)
-                markLand(grid, i, 0);
-            
-            if(grid[i][n-1] == 0)
-                markLand(grid, i, n-1);
-        }
-        
-        for(int i=0; i<n; i++){
-            if(grid[0][i] == 0)
-                markLand(grid, 0, i);
-            
-            if(grid[m-1][i] == 0)
-                markLand(grid, m-1, i);
-        }
-        
-        
-        int closedIslands = 0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 0){
-                    closedIslands++;
-                    markLand(grid, i, j);
+        int n = grid.length;
+        int m = grid[0].length;;
+        int count = 0;
+        for (int row=0; row < n;row++) {
+            for (int col=0;col<m;col++) {
+                if (grid[row][col] == 0 && !dfs(row, col, grid)) {
+                    count++;
                 }
             }
         }
+        return count;
+    }
+    
+// return true if cell touches the wall, or connected to another cell touching the wall
+    boolean dfs(int row, int col, int[][] grid) { 
+        if (grid[row][col] == 0) {
+            grid[row][col] = 2; // mark as visited
         
-        return closedIslands;
+            boolean up = row == 0 || dfs(row-1, col, grid);
+            boolean down = row == grid.length-1 || dfs(row+1, col, grid);
+            boolean left = col ==0 || dfs(row, col - 1, grid);
+            boolean right = col == grid[0].length-1 || dfs(row, col + 1, grid);
+
+            return up || down || left || right;
+        }
+        return false;
     }
 }
