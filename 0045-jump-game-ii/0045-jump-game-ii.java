3dbfc3569ef7a404c5abf6a1ea7f70f43bd4d083
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-05 13:53:30 -0400

    Time: 1 ms (99.50%), Space: 44.8 MB (21.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] jumps = new int[nums.length];
-    
-        Arrays.fill(jumps, Integer.MAX_VALUE);
-        int steps = Integer.MAX_VALUE;
-        jumps[0] = 0;
+        // The starting range of the first jump is [0, 0]
+        int answer = 0, n = nums.length;
+        int curEnd = 0, curFar = 0;
         
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i; j < nums.length && j <= i + nums[i]; j++){
-                
-                jumps[j] = Math.min(jumps[j], 1 + jumps[i]);
-                
-                if(j == nums.length - 1){
-                    steps = Math.min(jumps[j], steps);
-                }
-                
+        for (int i = 0; i < n - 1; ++i) {
+            // Update the farthest reachable index of this jump.
+            curFar = Math.max(curFar, i + nums[i]);
+
+            // If we finish the starting range of this jump,
+            // Move on to the starting range of the next jump.
+            if (i == curEnd) {
+                answer++;
+                curEnd = curFar;
             }
         }
         
-        return steps;
+        return answer;
     }
 }
