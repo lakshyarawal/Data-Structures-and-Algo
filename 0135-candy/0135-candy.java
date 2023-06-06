7d9ac5b00d11709897d39c666574bf35a58df2cb
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-06 13:21:06 -0400

    Time: 4 ms (21.72%), Space: 44.1 MB (17.35%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,36 @@
 public class Solution {
+    public int count(int n) {
+        return (n * (n + 1)) / 2;
+    }
     public int candy(int[] ratings) {
-        int[] candies = new int[ratings.length];
-        Arrays.fill(candies, 1);
+        if (ratings.length <= 1) {
+            return ratings.length;
+        }
+        int candies = 0;
+        int up = 0;
+        int down = 0;
+        int oldSlope = 0;
         for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+            int newSlope = (ratings[i] > ratings[i - 1]) ? 1 
+                : (ratings[i] < ratings[i - 1] ? -1 
+                : 0);
+
+            if ((oldSlope > 0 && newSlope == 0) || (oldSlope < 0 && newSlope >= 0)) {
+                candies += count(up) + count(down) + Math.max(up, down);
+                up = 0;
+                down = 0;
             }
-        }
-        int sum = candies[ratings.length - 1];
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            if (newSlope > 0) {
+                up++;
+            } else if (newSlope < 0) {
+                down++;
+            } else {
+                candies++;
             }
-            sum += candies[i];
+
+            oldSlope = newSlope;
         }
-        return sum;
+        candies += count(up) + count(down) + Math.max(up, down) + 1;
+        return candies;
     }
 }
