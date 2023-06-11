3ffe8d549a17b13931c7e441104fe1e78b9f7fb7
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-11 19:51:52 -0400

    Time: 2 ms (100.00%), Space: 43.4 MB (31.22%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -9,24 +9,6 @@
  * }
  */
 class Solution {
-    public ListNode addNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(0);
-        ListNode curr = head;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (l1 != null)
-                l1 = l1.next;
-            if (l2 != null)
-                l2 = l2.next;
-        }
-        return head.next;
-    }
     
     public ListNode reverseList(ListNode l1){
         ListNode curr = l1;
@@ -42,11 +24,28 @@ class Solution {
     }
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode headFirst = reverseList(l1);
-        ListNode headSecond = reverseList(l2);
+        ListNode r1 = reverseList(l1);
+        ListNode r2 = reverseList(l2);
         
-        ListNode finalNode = addNumbers(headFirst, headSecond);
-        
-        return reverseList(finalNode);
+        int totalSum = 0, carry = 0;
+        ListNode ans = new ListNode();
+        while (r1 != null || r2 != null) {
+            if (r1 != null) totalSum += r1.val;
+            if (r2 != null) totalSum += r2.val;
+            
+            ans.val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode head = new ListNode(carry);
+            head.next = ans;
+            ans = head;
+            totalSum = carry;
+
+            r1 = r1 != null ? r1.next : null;
+            r2 = r2 != null ? r2.next : null;
+        }
+
+        return carry == 0 ? ans.next: ans;
+        // ListNode finalNode = addNumbers(headFirst, headSecond);
+        // return reverseList(finalNode);
     }
 }
