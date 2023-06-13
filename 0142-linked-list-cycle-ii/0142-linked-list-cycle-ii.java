97b9382720f323848f8ecbb645e1f2684a1ea387
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-13 03:59:59 -0400

    Time: 0 ms (100.00%), Space: 44.1 MB (14.66%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,36 +11,28 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // Initialize tortoise and hare pointers
-        ListNode tortoise = head;
-        ListNode hare = head;
-
-        // Move tortoise one step and hare two steps
-        while (hare != null && hare.next != null) {
-            tortoise = tortoise.next;
-            hare = hare.next.next;
-
-            // Check if the hare meets the tortoise
-            if (tortoise == hare) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
                 break;
             }
         }
-
-        // Check if there is no cycle
-        if (hare == null || hare.next == null) {
+        
+        if(fast == null || fast.next == null){
             return null;
         }
-
-        // Reset either tortoise or hare pointer to the head
-        tortoise = head;
-
-        // Move both pointers one step until they meet again
-        while (tortoise != hare) {
-            tortoise = tortoise.next;
-            hare = hare.next;
+        
+        slow = head;
+        
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
         }
-
-        // Return the node where the cycle begins
-        return tortoise;
+        return slow;
     }
 }
