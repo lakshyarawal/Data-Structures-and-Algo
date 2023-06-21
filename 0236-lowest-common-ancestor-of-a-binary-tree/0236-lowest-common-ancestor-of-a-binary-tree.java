ab31acd2051d6b92a0d21e2a80316fd9025c773f
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-21 05:57:24 -0400

    Time: 7 ms (99.88%), Space: 43.6 MB (76.92%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,51 +9,23 @@
  */
 class Solution {
     
-    public boolean populate(TreeNode root, TreeNode curr, Deque<TreeNode> list){
-        if(root == curr){
-            list.offer(curr);
-            return true;
-        }
-        if(root == null){
-            return false;
-        }
-        if(populate(root.left, curr, list)){
-            list.offer(root.left);
-            return true;
-        }
-        else if(populate(root.right, curr, list)){
-            list.offer(root.right);
-            return true;
-        }
-        return false;
-    }
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Deque<TreeNode> list1 = new ArrayDeque<>();
-        Deque<TreeNode> list2 = new ArrayDeque<>();
-
-        if(populate(root, p, list1)){
-            list1.offer(root);
+        if(root == null || root == p || root == q){
+            return root;
         }
         
-        if(populate(root, q, list2)){
-            list2.offer(root);
-        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        while(!list1.isEmpty() && !list2.isEmpty()){
-            if(list1.size() > list2.size()){
-                list1.pop();
-            }
-            else if(list2.size() > list1.size()){
-                list2.pop();
-            }
-            else{
-                TreeNode node = list1.pop();
-                if(node == list2.pop()){
-                    return node;
-                }
-            }
+        if(left == null){
+            return right;
         }
-        return null;
+        else if(right == null){
+            return left;
+        }
+        else{
+            return root;
+        }
     }
 }
