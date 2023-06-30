656ac0eab14b2cb61dcfceac4146843e242e1990
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-06-30 01:20:28 -0400

    Time: 0 ms (100.00%), Space: 39.2 MB (75.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1){
+        if(n <= 1){
             return 1;
         }
-        int[] numWays = new int[n];
-        numWays[0] = 1;
-        numWays[1] = 2;
-        for(int i=2; i < n; i += 1){
-            numWays[i] = numWays[i-2] + numWays[i-1];
+        
+        int a = 1;
+        int b = 2;
+        
+        for(int i = 3; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        return numWays[n-1];
+        return b;
     }
 }
