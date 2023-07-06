79870fa8ff8e40c5e1679365f79420f8a6d557dd
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-06 06:08:37 -0400

    Time: 20 ms (81.25%), Space: 43.7 MB (71.88%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
@@ -1,57 +1,36 @@
 class Solution {
-    
-    class Bug {
-        int pos;
-        int direction;
-        Bug(int v, int d){
-            this.pos = v;
-            this.direction = d;
-        }
-    }
-    
     public int minimumJumps(int[] forbidden, int a, int b, int x) {
-        if(x == 0) return 0;
-        int steps = 0;
-        int furthest = 100000;
-        Queue<Bug> q = new LinkedList<>();
-        HashSet<Integer> forbiddenPositions = new HashSet<>();
-        HashSet<String> visitedSet = new HashSet<>();
-        
-        for(int el: forbidden){
-            forbiddenPositions.add(el);
+        Queue<int[]> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        for(int forbid : forbidden){
+            visited.add(forbid);
         }
-        
-        q.offer(new Bug(0, 0));
-        visitedSet.add(0+"-"+1);
-        steps++;
+        Set<Integer> stepsTaken = new HashSet<>();
+        q.offer(new int[]{0,0,1});//node,steps,forward
         
         while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int i = 0; i<size; i++){
-                Bug bug = q.poll();
-                
-                int nextPosition = bug.pos+a;
-                if(nextPosition == x){
-                    return steps;
-                }
-                if(nextPosition >= 0 && nextPosition < furthest && !forbiddenPositions.contains(nextPosition) && visitedSet.add(nextPosition+"-"+1)){
-                    q.offer(new Bug(nextPosition, 1));
-                }
-                    if(bug.direction == 1){
-                        int backPosition = bug.pos - b;
-                        if(backPosition == x){
-                            return steps;
-                        }
-                        if(backPosition >= 0 && backPosition < furthest && !forbiddenPositions.contains(backPosition) && visitedSet.add(backPosition+"-"+0)){
-                            q.offer(new Bug(backPosition, 0));
-                        }
-                    }
+            int[] val = q.poll();
+            int node = val[0];
+            int steps = val[1];
+            int isForward = val[2];
+            if(node==x){
+                return steps;
             }
-            steps++;
+            if(visited.contains(node)){
+                continue;
+            }
+            if(isForward==1){
+                //can visit forward and backward both
+                int backStep = node-b;
+                if(backStep>=0 && stepsTaken.add(backStep)){
+                 q.offer(new int[]{backStep,steps+1,-1});
+                }
+            }
+            int forwardStep = node+a;
+            if(forwardStep>=0 && forwardStep<(2000+2*b) && stepsTaken.add(forwardStep)){
+                q.offer(new int[]{forwardStep,steps+1,1});
+            }
         }
-        
         return -1;
-        
     }
 }
