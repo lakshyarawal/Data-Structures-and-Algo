899dc8ef42e9b6fdc4424b2390ffbda46eb1cdcd
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-07 07:23:19 -0400

    Time: 3 ms (27.70%), Space: 40.1 MB (95.08%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        String currentLCP = strs[0];
+        int lcpLength = strs[0].length();
+        
+        HashMap<Integer, Character> lcpMap = new HashMap<>();
+        for(int j = 0; j < strs[0].length(); j++){
+            lcpMap.put(j, strs[0].charAt(j));
+        }
+        
+        for(int i = 1; i < strs.length; i++){
+            int newLcpLength = 0;
+            for(int j = 0; j < strs[i].length(); j++){
+                if(lcpMap.containsKey(j) && strs[i].charAt(j) == lcpMap.get(j)){
+                    newLcpLength++;
+                }else{
+                    break;
+                }
+            }
+            lcpLength = Math.min(lcpLength, newLcpLength);
+            if(lcpLength == 0) return "";
+        }
+        
+        return currentLCP.substring(0, lcpLength);
+    }
+}
