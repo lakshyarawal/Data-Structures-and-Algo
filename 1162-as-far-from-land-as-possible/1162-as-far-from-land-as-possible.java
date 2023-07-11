7cf9abd361d09550ff07001b529e331e93ddfc44
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-11 12:48:59 -0400

    Time: 7 ms (98.55%), Space: 43.9 MB (85.63%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -3,30 +3,25 @@ class Solution {
         int n = grid.length;
         int maxDistance = n + n + 1;
         
-        int[][] distance = new int[n][n];
-        for (int[] arr : distance) Arrays.fill(arr, maxDistance);
-        
         for(int i = 0; i <n; i++){
             for(int j = 0; j< n; j++){
                 if(grid[i][j] == 1){
-                    distance[i][j] = 0;
+                    grid[i][j] = 0;
                 } else{
-                    distance[i][j] = Math.min(Math.min(i-1 >= 0 ? distance[i-1][j] +1 : maxDistance, j-1 >=0 ? distance[i][j-1] + 1: maxDistance), distance[i][j]);
+                    grid[i][j] = maxDistance;
+                    grid[i][j] = Math.min(Math.min(i-1 >= 0 ? grid[i-1][j] +1 : maxDistance, j-1 >=0 ? grid[i][j-1] + 1: maxDistance), grid[i][j]);
                 }
             }
         }
         int dist = Integer.MIN_VALUE;
         for(int i = n-1 ; i >= 0; i--){
             for(int j = n-1; j>=0; j--){
-                if(grid[i][j] == 1){
-                    distance[i][j] = 0;
-                } else{
-                    distance[i][j] = Math.min(Math.min(i < n-1 ? distance[i+1][j]+1 : maxDistance, j < n-1 ? distance[i][j+1]+1: maxDistance), distance[i][j]);
-                    dist = Math.max(dist, distance[i][j]);
-                }
+                    grid[i][j] = Math.min(Math.min(i < n-1 ? grid[i+1][j]+1 : maxDistance, j < n-1 ? grid[i][j+1]+1: maxDistance), grid[i][j]);
+                    dist = Math.max(dist, grid[i][j]);
+                
             }
         }
         
-        return dist == maxDistance || dist == Integer.MIN_VALUE ? -1: dist;
+        return dist == maxDistance || dist == 0 ? -1: dist;
     }
 }
