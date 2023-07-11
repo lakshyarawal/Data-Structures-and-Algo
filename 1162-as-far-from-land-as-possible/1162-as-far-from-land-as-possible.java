ba4673b051c0c285faf3a193420b779409fb43b9
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-11 12:45:19 -0400

    Time: 8 ms (96.95%), Space: 44.4 MB (49.64%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,39 +1,32 @@
 class Solution {
     public int maxDistance(int[][] grid) {
         int n = grid.length;
-        int[][] visited = new int[n][n];
-        int[][] directions = new int[][]{{-1, 0} , {1, 0} , {0, 1} , {0, -1}};
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int maxDistance = n + n + 1;
+        
+        int[][] distance = new int[n][n];
+        for (int[] arr : distance) Arrays.fill(arr, maxDistance);
+        
         for(int i = 0; i <n; i++){
-            for(int j =0; j < n; j++){
+            for(int j = 0; j< n; j++){
                 if(grid[i][j] == 1){
-                    visited[i][j] = 1;
-                    q.offer(new Pair(i, j));
+                    distance[i][j] = 0;
+                } else{
+                    distance[i][j] = Math.min(Math.min(i-1 >= 0 ? distance[i-1][j] +1 : maxDistance, j-1 >=0 ? distance[i][j-1] + 1: maxDistance), distance[i][j]);
+                }
+            }
+        }
+        int dist = Integer.MIN_VALUE;
+        for(int i = n-1 ; i >= 0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(grid[i][j] == 1){
+                    distance[i][j] = 0;
+                } else{
+                    distance[i][j] = Math.min(Math.min(i < n-1 ? distance[i+1][j]+1 : maxDistance, j < n-1 ? distance[i][j+1]+1: maxDistance), distance[i][j]);
+                    dist = Math.max(dist, distance[i][j]);
                 }
             }
         }
         
-        int distance = -1;
-        
-        while(!q.isEmpty()){
-            int qSize = q.size();
-            for(int i = 0; i< qSize; i++){
-                Pair<Integer, Integer> landCell = q.poll();
-                int r = landCell.getKey();
-                int c = landCell.getValue();
-
-                for(int[] d : directions){
-                    int x = r + d[0];
-                    int y = c + d[1];
-                    if(x >= 0 && y >= 0 && x < n && y < n && visited[x][y] == 0){
-                        visited[x][y] = 1;
-                        q.offer(new Pair(x, y));
-                    }
-                }  
-            }
-            distance++; 
-        }
-        
-        return distance == 0 ? -1: distance;
+        return dist == maxDistance || dist == Integer.MIN_VALUE ? -1: dist;
     }
 }
