8d70ea6e81325040b350eeda9f083af4af8c71ba
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-12 01:11:07 -0400

    Time: 3 ms (86.63%), Space: 43.3 MB (73.89%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,24 +1,16 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if(nums.length ==0){
-            return 0;
+        int value[] = new int[10001];
+        for(int i=0; i<nums.length; i++){
+            value[nums[i]] += nums[i];
         }
-        int maxNum = Integer.MIN_VALUE;
-        for(int el: nums){
-            maxNum = Math.max(maxNum, el);
+        int prev1, prev2;
+        prev1 = prev2 = 0;
+        for(int i=0; i<value.length; i++){
+            int dp = Math.max(prev1, prev2 + value[i]);
+            prev2 = prev1;
+            prev1 = dp;
         }
-        int [] freq =  new int[maxNum + 1]; 
-        for(int el: nums){
-           freq[el] += el;
-        }
-
-        int[] dp = new int[freq.length];
-        dp[0] = 0;
-        dp[1] = freq[1];
-        for(int i = 2; i< freq.length; i++)
-            dp[i] = Math.max(freq[i] + dp[i-2], dp[i-1]);
-
-        return dp[freq.length-1];
-        
+        return prev1;
     }
 }
