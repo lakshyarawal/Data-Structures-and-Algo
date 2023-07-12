e131fd90f00191cfb218458ced8d5a5a3fb4855f
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-12 08:54:58 -0400

    Time: 3 ms (99.87%), Space: 43.3 MB (44.94%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,29 +1,37 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        int result = nums[0] + nums[1] + nums[nums.length -1];
+public int threeSumClosest(int[] nums, int target) {
+        if(nums==null || nums.length<3)     return 0;
+        if(nums.length==3)                  return nums[0]+nums[1]+nums[2];
         Arrays.sort(nums);
-        int sum = 0;
-        int diff = 0;
-
-        for(int i = 0;i<nums.length - 2;i++){
-            int ptr1 = i + 1;
-            int ptr2 = nums.length -1;
-
-            while(ptr2 > ptr1){
-                sum = nums[i] + nums[ptr1] + nums[ptr2];
-                if(sum > target){
-                    ptr2--;
+        int n=nums.length;
+        int sum=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<n-2;i++){
+            if(nums[i]+nums[n-1]+nums[n-2]<target){
+                sum=nums[i]+nums[n-1]+nums[n-2];
+                continue;
+            }
+            if(nums[i]+nums[i+1]+nums[i+2]>target){
+                int temp=nums[i]+nums[i+1]+nums[i+2];
+                return lessGap(sum,temp,target);
+            }
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int temp=nums[i]+nums[j]+nums[k];
+                if(temp==target)            return target;
+                if(temp<target){
+                    j++;
+                    sum=lessGap(sum,temp,target);
                 }
                 else{
-                    ptr1++;
+                    k--;
+                    sum=lessGap(sum,temp,target);
                 }
-                if(Math.abs(sum - target) < Math.abs(result - target)){
-                    result = sum;
-                }
-
             }
         }
-        return result;
-        
+        return sum;        
+    }
+    private int lessGap(int sum,int temp,int target){
+        return Math.abs(sum-target)<Math.abs(temp-target) ? sum : temp;
     }
 }
