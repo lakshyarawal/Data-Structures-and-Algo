0d52e4f764656191ec7eb7a282c6a8c7c6efe4e0
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-12 08:52:45 -0400

    Time: 13 ms (95.62%), Space: 43.3 MB (44.94%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,19 +1,53 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        int diff = Integer.MAX_VALUE;
-        int sz = nums.length;
+ public int threeSumClosest(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = Integer.MAX_VALUE; //Diff
+        result[1] = Integer.MIN_VALUE;//sum
+        if (nums == null || nums.length < 3) {
+            return result[1];
+        } 
+        
         Arrays.sort(nums);
-        for (int i = 0; i < sz && diff != 0; ++i) {
-            for (int j = i + 1; j < sz - 1; ++j) {
-                int complement = target - nums[i] - nums[j];
-                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
-                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
-                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
-                    diff = complement - nums[hi];
-                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
-                    diff = complement - nums[lo];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int[] temp = new int[2];
+            temp[0] = result[0];
+            temp[1] = result[1];
+            twoSumClosest(nums, i + 1, nums.length - 1, target - nums[i], temp);
+            if (temp[0] < result[0]) {
+                temp[1] += nums[i];
+                result = temp;
+            }
+            if(result[0] == 0) {
+                break;
+            }
+        } 
+        return result[1];
+    }
+    
+    private void twoSumClosest(int[] nums, int start, int end, int target, int[] result) {
+        int left = start;
+        int right = end;
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) {
+                if (target - sum < result[0]) {
+                    result[0] = target - sum;
+                    result[1] = sum;
+                }
+                left++;
+            } else if (nums[left] + nums[right] > target) {
+                if (sum - target < result[0]) {
+                    result[0] = sum - target;
+                    result[1] = sum;
+                } 
+                right--;
+            } else {
+                result[0] = 0;
+                result[1] = target;
+                break;
             }
         }
-        return target - diff;
     }
 }
