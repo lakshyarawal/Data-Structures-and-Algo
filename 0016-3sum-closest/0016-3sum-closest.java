65f3a5efe353d6c5383e9eabf975c8e93a7dc1f4
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-12 08:48:08 -0400

    Time: 82 ms (9.85%), Space: 43.3 MB (44.94%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,32 +1,19 @@
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums); // Sort the array to optimize the two-pointer approach
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize the closest sum
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            int left = i + 1; // Pointer from the next element
-            int right = nums.length - 1; // Pointer from the last element
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == target) {
-                    return target; // Found an exact match, return the target sum
-                }
-
-                // Update the closest sum if the current sum is closer to the target
-                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                    closestSum = sum;
-                }
-
-                if (sum < target) {
-                    left++; // Move the left pointer to increase the sum
-                } else {
-                    right--; // Move the right pointer to decrease the sum
-                }
+        int diff = Integer.MAX_VALUE;
+        int sz = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < sz && diff != 0; ++i) {
+            for (int j = i + 1; j < sz - 1; ++j) {
+                int complement = target - nums[i] - nums[j];
+                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
+                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
+                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
+                    diff = complement - nums[hi];
+                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
+                    diff = complement - nums[lo];
             }
         }
-
-        return closestSum;
+        return target - diff;
     }
 }
