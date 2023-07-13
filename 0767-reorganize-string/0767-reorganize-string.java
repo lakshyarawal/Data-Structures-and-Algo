075c72e804e869dc4f7ea6289633810db9140e7c
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-13 04:43:51 -0400

    Time: 4 ms (55.69%), Space: 40.6 MB (79.36%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,29 +1,51 @@
 class Solution {
-    public String reorganizeString(String S) {
-        int arr[]=new int[26];
-        //encoded char in int array(as: 'a'->0,'b'->1,'c'->2----------------'z'->25)
-        for(char c : S.toCharArray()) arr[c-'a']++;
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
-        for(int i=0;i<26;i++)
-            if(arr[i]!=0) pq.add(new int[]{i,arr[i]});
-        int last=-1;
-        StringBuilder str=new StringBuilder();
-        while(!pq.isEmpty()){
-            int a[]=pq.poll();
-            if(a[0]!=last){
-                last=a[0];
-                if(a[1]>1)pq.add(new int[]{a[0],a[1]-1});
-            }
-            else if(pq.isEmpty()) return "";
-                 else{
-                     int a2[]=pq.poll();
-                     last=a2[0];
-                     if(a2[1]>1) pq.add(new int[]{a2[0],a2[1]-1});
-                     pq.add(a);
-                 }
-            //decode char from int 
-            str.append((char)(last+'a'));
+    public String reorganizeString(String s) {
+        Map<Character, Integer> charFreq = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
         }
-        return str.toString();
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (Map.Entry<Character, Integer> e : charFreq.entrySet()) {
+            pq.add(new Pair(e.getKey(), e.getValue()));
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Pair first = pq.poll();
+            sb.append(first.c);
+            Pair second = pq.poll();
+            if (second != null) {
+                sb.append(second.c);
+            } else if (first.freq > 1) {
+                return "";
+            }
+            // Important to put first before second in the queue. Otherwise if frequencies are equal in the next step the second will be polled before first.
+            if (first.freq > 1) {
+                first.freq--;
+                pq.add(first);
+            }
+            if (second != null && second.freq > 1) {
+                second.freq--;
+                pq.add(second);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static class Pair implements Comparable<Pair> {
+        char c;
+        int freq;
+
+        public Pair(char c, int freq) {
+            this.c = c;
+            this.freq = freq;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return Integer.compare(o.freq, this.freq);
+        }
     }
 }
