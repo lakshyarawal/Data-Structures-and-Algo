5fe92b9860e3dc18ccaa986c329d49fd1f318518
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-13 09:40:39 -0400

    Time: 1 ms (12.30%), Space: 41.3 MB (5.74%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countNeighbors(int r, int c, int[][] board){
+        int neighbors = 0;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+        for(int[] dir : directions){
+            int newRow = r + dir[0];
+            int newCol = c + dir[1];
+            if(newRow < board.length && newRow >= 0 && newCol < board[0].length && newCol >= 0){
+                if(board[newRow][newCol] == 1)neighbors++;
+            }
+        }
+        return neighbors;
+    }
+    public void gameOfLife(int[][] board) {
+        
+        int m = board.length;
+        int n = board[0].length;
+        int[][] nextStateBoard = new int[m][n];
+        
+        for(int i = 0; i< m; i++){
+            for(int j = 0; j < n; j++){
+                int neighbors = countNeighbors(i, j, board);
+                //Condn 1
+                if(board[i][j] == 1 && (neighbors < 2 || neighbors > 3)){
+                    nextStateBoard[i][j] = 0;
+                } else if(board[i][j] == 0 && neighbors == 3){
+                    nextStateBoard[i][j] = 1;
+                } else{
+                    nextStateBoard[i][j] = board[i][j];
+                }
+            }
+        }
+        
+        for(int i = 0; i< m; i++){
+            for(int j = 0; j < n; j++){
+                 board[i][j] = nextStateBoard[i][j];
+            }
+        }
+        
+    }
+}
