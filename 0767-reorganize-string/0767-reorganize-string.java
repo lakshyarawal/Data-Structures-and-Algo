e6c5904dc94cb4f07a1d15c5f316091c4895b273
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-13 04:40:21 -0400

    Time: 3 ms (66.83%), Space: 40.8 MB (68.30%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,41 +1,29 @@
 class Solution {
-    public String reorganizeString(String s) {
-        var charCounts = new int[26];
-        for (char c : s.toCharArray()) {
-            charCounts[c - 'a'] = charCounts[c - 'a'] + 1;
-        }
-
-        // Max heap ordered by character counts
-        var pq = new PriorityQueue<int[]>((a, b) -> Integer.compare(b[1], a[1]));
-        for (int i = 0; i < 26; i++) {
-            if (charCounts[i] > 0) {
-                pq.offer(new int[] {i + 'a', charCounts[i]});
+    public String reorganizeString(String S) {
+        int arr[]=new int[26];
+        //encoded char in int array(as: 'a'->0,'b'->1,'c'->2----------------'z'->25)
+        for(char c : S.toCharArray()) arr[c-'a']++;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int i=0;i<26;i++)
+            if(arr[i]!=0) pq.add(new int[]{i,arr[i]});
+        int last=-1;
+        StringBuilder str=new StringBuilder();
+        while(!pq.isEmpty()){
+            int a[]=pq.poll();
+            if(a[0]!=last){
+                last=a[0];
+                if(a[1]>1)pq.add(new int[]{a[0],a[1]-1});
             }
+            else if(pq.isEmpty()) return "";
+                 else{
+                     int a2[]=pq.poll();
+                     last=a2[0];
+                     if(a2[1]>1) pq.add(new int[]{a2[0],a2[1]-1});
+                     pq.add(a);
+                 }
+            //decode char from int 
+            str.append((char)(last+'a'));
         }
-            
-        var sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            var first = pq.poll();
-            if (sb.length() == 0 || first[0] != sb.charAt(sb.length() - 1)) {
-                sb.append((char) first[0]);
-                if (--first[1] > 0) {
-                    pq.offer(first);
-                }
-            } else {
-                if (pq.isEmpty()) {
-                    return "";
-                }
-                
-                var second = pq.poll();
-                sb.append((char) second[0]);
-                if (--second[1] > 0) {
-                    pq.offer(second);
-                }
-                
-                pq.offer(first);
-            }
-        }
-        
-        return sb.toString();
+        return str.toString();
     }
 }
