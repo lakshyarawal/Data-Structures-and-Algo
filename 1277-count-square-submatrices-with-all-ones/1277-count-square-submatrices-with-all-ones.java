a0fd045f37363a0b8bc76ee7d399971e3fe2b593
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-14 08:41:51 -0400

    Time: 7 ms (40.02%), Space: 54.2 MB (76.18%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,31 +1,20 @@
 class Solution {
-    int rows, cols;
-    public boolean checkSquare(int r, int c, int size, int[][] matrix){
-        if(r + size >= rows) return false;
-        if(c + size >= cols) return false;
-        for(int i = r; i <= r + size; i++){
-            for(int j = c; j <= c+ size; j++){
-                if(matrix[i][j] != 1) return false;
-            }
-        }
-        return true;
-    }
-    
     public int countSquares(int[][] matrix) {
-        int totalSquares = 0;
-        rows = matrix.length;
-        cols = matrix[0].length;
-        int maxSize = Math.min(rows, cols);
-        for(int i = 0;i < maxSize; i++){
-            for(int j = 0; j< rows; j++){
-                for(int k = 0; k < cols; k++){
-                    if(checkSquare(j, k, i, matrix)){
-                        totalSquares++;
-                    }
+        int res = 0;
+        
+        for(int r = 0; r< matrix.length; r++){
+            for(int c = 0; c < matrix[0].length; c++){
+                if(matrix[r][c] > 0 && r > 0 && c > 0){
+                    matrix[r][c] = Math.min(matrix[r-1][c], Math.min(matrix[r][c-1], matrix[r-1][c-1])) + 1;
                 }
+                 res += matrix[r][c];
+                    
             }
-            //System.out.println("For size: "+ (i+1) + " Total Squares are: "+ totalSquares);
+
         }
-        return totalSquares;
+        
+            //System.out.println("For size: "+ (i+1) + " Total Squares are: "+ totalSquares);
+        
+        return res;
     }
 }
