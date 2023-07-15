4f53aee8aac79a7fae8d8f80bdc176e10f35f471
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-15 05:48:26 -0400

    Time: 21 ms (74.34%), Space: 44.2 MB (40.86%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,59 +1,47 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        return kSum(nums, target, 0, 4);
-    }
-	
-    public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length - 1;
 
-        // If we have run out of numbers to add, return res.
-        if (start == nums.length) {
-            return res;
-        }
-        
-        // There are k remaining values to add to the sum. The 
-        // average of these values is at least target / k.
-        long average_value = target / k;
-        
-        // We cannot obtain a sum of target if the smallest value
-        // in nums is greater than target / k or if the largest 
-        // value in nums is smaller than target / k.
-        if  (nums[start] > average_value || average_value > nums[nums.length - 1]) {
-            return res;
-        }
-        
-        if (k == 2) {
-            return twoSum(nums, target, start);
-        }
-    
-        for (int i = start; i < nums.length; ++i) {
-            if (i == start || nums[i - 1] != nums[i]) {
-                for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
-                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
-                    res.get(res.size() - 1).addAll(subset);
+        Arrays.sort(nums);
+
+        for(int i=0; i<=n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            for(int j=i+1; j<=n; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+
+                int k = j+1;
+                int l = n;
+
+                while(k < l){
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if(sum == target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    }else if(sum < target){
+                        k++;
+                    }else{
+                        l--;
+                    }
+
                 }
             }
         }
-    
-        return res;
-    }
-	
-    public List<List<Integer>> twoSum(int[] nums, long target, int start) {
-        List<List<Integer>> res = new ArrayList<>();
-        int lo = start, hi = nums.length - 1;
-    
-        while (lo < hi) {
-            int currSum = nums[lo] + nums[hi];
-            if (currSum < target || (lo > start && nums[lo] == nums[lo - 1])) {
-                ++lo;
-            } else if (currSum > target || (hi < nums.length - 1 && nums[hi] == nums[hi + 1])) {
-                --hi;
-            } else {
-                res.add(Arrays.asList(nums[lo++], nums[hi--]));
-            }
-        }
-                                                          
-        return res;
+
+        return ans;
     }
 }
