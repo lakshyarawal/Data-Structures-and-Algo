898b5e4c2b8a792743fa3e9699e92d40c8c2b627
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-15 08:43:03 -0400

    Time: 12 ms (64.12%), Space: 56.8 MB (79.96%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,24 +1,20 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        // totalTime: total time needed to make rope colorful;
-        // currMaxTime: maximum time of a balloon needed.
-        int totalTime = 0, currMaxTime = 0;
         
-        // For each balloon in the array:
-        for (int i = 0; i < colors.length(); ++i) {
-            // If this balloon is the first balloon of a new group
-            // set the currMaxTime as 0.
-            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
-                currMaxTime = 0;
+        int lastTime = neededTime[0];
+        int res = 0;
+        
+        for(int i = 1; i< colors.length(); i++){
+            if(colors.charAt(i) == colors.charAt(i-1)){
+                
+                res += Math.min(lastTime, neededTime[i]);
+                lastTime = Math.max(neededTime[i], lastTime);
+                //System.out.println("RES: " + res);
+            } else{
+                lastTime = neededTime[i];
             }
             
-            // Increment totalTime by the smaller one.
-            // Update currMaxTime as the larger one.
-            totalTime += Math.min(currMaxTime, neededTime[i]);
-            currMaxTime = Math.max(currMaxTime, neededTime[i]);
         }
-        
-        // Return totalTime as the minimum removal time.
-        return totalTime;
+        return res;
     }
 }
