b4645a27c53a3b4b7995fb3f10a294eab63c2a62
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-15 08:37:32 -0400

    Time: 14 ms (40.27%), Space: 56.7 MB (83.21%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        // Initalize two pointers i, j.
-        int totalTime = 0;
-        int i = 0, j = 0;
+        // totalTime: total time needed to make rope colorful;
+        // currMaxTime: maximum time of a balloon needed.
+        int totalTime = 0, currMaxTime = 0;
         
-        while (i < neededTime.length && j < neededTime.length) {
-            int currTotal = 0, currMax = 0;
-            
-            // Find all the balloons having the same color as the 
-            // balloon indexed at i, record the total removal time 
-            // and the maximum removal time.
-            while (j < neededTime.length && colors.charAt(i) == colors.charAt(j)) {
-                currTotal += neededTime[j];
-                currMax = Math.max(currMax, neededTime[j]);
-                j++;
+        // For each balloon in the array:
+        for (int i = 0; i < colors.length(); ++i) {
+            // If this balloon is the first balloon of a new group
+            // set the currMaxTime as 0.
+            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+                currMaxTime = 0;
             }
             
-            // Once we reach the end of the current group, add the cost of 
-            // this group to total_time, and reset two pointers.
-            totalTime += currTotal - currMax;
-            i = j;
+            // Increment totalTime by the smaller one.
+            // Update currMaxTime as the larger one.
+            totalTime += Math.min(currMaxTime, neededTime[i]);
+            currMaxTime = Math.max(currMaxTime, neededTime[i]);
         }
-        return totalTime;    
+        
+        // Return totalTime as the minimum removal time.
+        return totalTime;
     }
 }
