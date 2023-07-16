6bf4003ab7f0b5505d76ab15dfa8e119affc952a
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-16 00:56:49 -0400

    Time: 193 ms (60.00%), Space: 59.6 MB (40.00%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.java
@@ -1,32 +1,39 @@
 class Solution {
-    public int longestValidSubstring(String word, List<String> forbidden) {
-        int n = word.length();
-        int curr = 0;
-        
-        
-        
-        Set<String> forbidSet = new HashSet<>(forbidden);
-        
-        int ans = 0;
-        
-        for(int i = n - 1; i >= 0; i--){
-            StringBuilder sb = new StringBuilder();
-            int len = 0;
-            int prev = curr;
-            for(int j = i; j < Math.min(n, i + 10); j++){
-                sb.append(word.charAt(j) + "");
-                if(forbidSet.contains(sb.toString())){
-                    break;
-                }
-                len++;
-            }
-            
-            if(len >= 10) len = n - i;
-            
-            curr = Math.min(1 + prev, len);
-            ans = Math.max(ans, curr);
-        }
-        
-        return ans;
+  int left = 0;
+  int right = 1;
+  int max = 0;
+  TreeMap<Integer, Set<String>> lengthToForbidden = new TreeMap<>();
+
+  public int longestValidSubstring(String word, List<String> forbidden) {
+    createLengthToForbidden(forbidden);
+    while (right < word.length()) {
+      checkInvalidSubstrings(word);
+      ++right;
     }
+    checkInvalidSubstrings(word);
+    return max;
+  }
+
+  private void createLengthToForbidden(List<String> forbidden) {
+    for (String forbiddenString : forbidden) {
+      int length = forbiddenString.length();
+      if (!lengthToForbidden.containsKey(length)) {
+        lengthToForbidden.put(length, new HashSet<>());
+      }
+      lengthToForbidden.get(length).add(forbiddenString);
+    }
+  }
+
+  private void checkInvalidSubstrings(String word) {
+    Integer forbiddenLength = lengthToForbidden.firstKey();
+    while (forbiddenLength != null && forbiddenLength <= right - left) {
+      String substring = word.substring(right - forbiddenLength, right);
+      if (lengthToForbidden.get(forbiddenLength).contains(substring)) {
+        left = right - forbiddenLength + 1;
+        break;
+      }
+      forbiddenLength = lengthToForbidden.higherKey(forbiddenLength);
+    }
+    max = Math.max(max, right - left);
+  }
 }
