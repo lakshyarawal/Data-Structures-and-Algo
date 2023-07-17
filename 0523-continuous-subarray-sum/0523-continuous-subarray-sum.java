8602519e0d09554be46c64b8716da2c9b22b57ed
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-17 04:50:28 -0400

    Time: 24 ms (37.84%), Space: 56.8 MB (78.13%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        // initialize the hash map with index 0 for sum 0
-        Map<Integer, Integer> hashMap = new HashMap<>(Map.of(0, 0));
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            // if the remainder sum % k occurs for the first time
-            if (!hashMap.containsKey(sum % k))
-                hashMap.put(sum % k, i + 1);
-            // if the subarray size is at least two
-            else if (hashMap.get(sum % k) < i)
-                return true;
+        Map<Integer, Integer> map = new HashMap<>(){{put(0,-1);}};;
+        int runningSum = 0;
+        for (int i=0;i<nums.length;i++) {
+            runningSum += nums[i];
+            if (k != 0) runningSum %= k; 
+            Integer prev = map.get(runningSum);
+            if (prev != null) {
+                if (i - prev > 1) return true;
+            }
+            else map.put(runningSum, i);
         }
         return false;
     }
