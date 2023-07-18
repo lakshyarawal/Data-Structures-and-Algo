165db7487ab5123cf859e5bcea027f524ff38057
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-18 06:05:23 -0400

    Time: 46 ms (70.99%), Space: 43.8 MB (51.21%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,22 +1,27 @@
 class Solution {
-    public static final int MOD = 1000000007;
-    public int knightDialer(int N) {
-        int[][] graph = new int[][]{{4,6},{6,8},{7,9},{4,8},{3,9,0},{},{1,7,0},{2,6},{1,3},{2,4}};
-        int cnt = 0;
-        Integer[][] memo = new Integer[N+1][10];
-        for (int i = 0; i <= 9; i++)
-            cnt = (cnt + helper(N-1, i, graph, memo)) % MOD;
-        return cnt;
-    }
-    private int helper(int N, int cur, int[][] graph, Integer[][] memo) {
-        if (N == 0)
-            return 1;
-        if (memo[N][cur] != null)
-            return memo[N][cur];
-        int cnt = 0;
-        for (int nei : graph[cur])
-            cnt = (cnt + helper(N-1, nei, graph, memo)) % MOD;
-        memo[N][cur] = cnt;
-        return cnt;
+    public int knightDialer(int N){
+        int[][] dirs = new int[][]{{4,6},{6,8},{7,9},{4,8},{3,9,0},{},{1,7,0},{2,6},{1,3},{2,4}};
+        
+        int[][] dp = new int[ N + 1][10];
+        for(int j = 0; j < dp[0].length; j ++){
+            dp[1][j] = 1;
+        }
+        int mod = (int)1e9 + 7;
+        for(int i = 2; i < dp.length;i ++)
+            for(int j = 0; j < dp[0].length; j ++){
+					
+                int[] dir = dirs[j]; 	//Where j comes from
+                for(int num : dir){
+                    dp[i][j] += dp[i - 1][num]; 
+                    dp[i][j] %= mod;
+                }
+            }
+        int count = 0;
+        for(int i = 0; i < dp[0].length; i ++){
+            count += dp[N][i];
+            count %= mod;
+        }
+       return count;
+       
     }
 }
