6370f04657f0e3c6b2ecfe160c58e6e7ceeaceef
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-18 06:06:03 -0400

    Time: 6 ms (98.90%), Space: 45 MB (21.54%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,27 +1,33 @@
 class Solution {
-    public int knightDialer(int N){
-        int[][] dirs = new int[][]{{4,6},{6,8},{7,9},{4,8},{3,9,0},{},{1,7,0},{2,6},{1,3},{2,4}};
-        
-        int[][] dp = new int[ N + 1][10];
-        for(int j = 0; j < dp[0].length; j ++){
-            dp[1][j] = 1;
-        }
-        int mod = (int)1e9 + 7;
-        for(int i = 2; i < dp.length;i ++)
-            for(int j = 0; j < dp[0].length; j ++){
-					
-                int[] dir = dirs[j]; 	//Where j comes from
-                for(int num : dir){
-                    dp[i][j] += dp[i - 1][num]; 
-                    dp[i][j] %= mod;
+    static int[][] lst = { {4,6}, {6,8}, {7,9}, {4,8}, {0,1,3}, {}, {0,1,7}, {2,6}, {1,3}, {2,4} };
+    static int[][] dp;
+    static boolean alreadyDone = false;
+    private static void calculate() {
+        dp = new int[50001][11];
+        Arrays.fill(dp[1], 1);
+        dp[1][10] = 10;
+        int MOD = 1000000007;
+        for(int i = 2; i <= 5000; i++) {
+            int res = 0;
+            for(int j = 0; j < 10; j++) {
+                if(j == 5) {
+                    continue;
                 }
+                for(int x : lst[j]) {
+                    dp[i][j] = (((dp[i][j]%MOD) + (dp[i-1][x]%MOD)) % MOD);
+                }
+                res = (((res%MOD) + (dp[i][j]%MOD)) % MOD); 
             }
-        int count = 0;
-        for(int i = 0; i < dp[0].length; i ++){
-            count += dp[N][i];
-            count %= mod;
+            dp[i][10] = res;
         }
-       return count;
-       
+    }
+    Solution() {
+        if(!alreadyDone) {
+            alreadyDone = true;
+            calculate();
+        }
+    }
+    public int knightDialer(int n) {
+        return dp[n][10];
     }
 }
