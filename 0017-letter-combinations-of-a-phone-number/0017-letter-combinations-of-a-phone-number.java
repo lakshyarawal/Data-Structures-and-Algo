babfce736ed353f84754888467d98b0343d0f74d
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-18 04:31:39 -0400

    Time: 1 ms (77.69%), Space: 41.1 MB (89.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,30 @@
 class Solution {
+    HashMap<Integer,String> hash=new HashMap<>();
+    List<String> list=new ArrayList<>();
+    public void dfs(String digits,StringBuilder sb,int i){
+        if(i==digits.length()){
+            list.add(sb.toString());
+            return;
+        }
+        for(char c:hash.get(Character.getNumericValue(digits.charAt(i))).toCharArray()){
+            sb.append(c);
+            dfs(digits,sb,i+1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
     public List<String> letterCombinations(String digits) {
-		LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
-	}
+        hash.put(2,"abc");
+        hash.put(3,"def");
+        hash.put(4,"ghi");
+        hash.put(5,"jkl");
+        hash.put(6,"mno");
+        hash.put(7,"pqrs");
+        hash.put(8,"tuv");
+        hash.put(9,"wxyz");
+        if(digits.length()==0){
+            return list;
+        }
+        dfs(digits,new StringBuilder(),0);
+        return list;
+    }
 }
