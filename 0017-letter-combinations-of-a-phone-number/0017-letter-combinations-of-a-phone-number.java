f9538ddc83a3dc960cee6433556617933cbecdd8
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-18 05:09:39 -0400

    Time: 1 ms (77.69%), Space: 40.9 MB (94.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,29 +1,32 @@
 class Solution {
-    HashMap<Integer,String> hash=new HashMap<>();
+    
+    HashMap<Character,String> hash=new HashMap<>();
     List<String> list=new ArrayList<>();
+    
     public void dfs(String digits,StringBuilder sb,int i){
         if(i==digits.length()){
             list.add(sb.toString());
             return;
         }
-        for(char c:hash.get(Character.getNumericValue(digits.charAt(i))).toCharArray()){
+        for(char c:hash.get(digits.charAt(i)).toCharArray()){
             sb.append(c);
             dfs(digits,sb,i+1);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
     public List<String> letterCombinations(String digits) {
-        hash.put(2,"abc");
-        hash.put(3,"def");
-        hash.put(4,"ghi");
-        hash.put(5,"jkl");
-        hash.put(6,"mno");
-        hash.put(7,"pqrs");
-        hash.put(8,"tuv");
-        hash.put(9,"wxyz");
         if(digits.length()==0){
             return list;
         }
+        hash.put('2',"abc");
+        hash.put('3',"def");
+        hash.put('4',"ghi");
+        hash.put('5',"jkl");
+        hash.put('6',"mno");
+        hash.put('7',"pqrs");
+        hash.put('8',"tuv");
+        hash.put('9',"wxyz");
+        
         dfs(digits,new StringBuilder(),0);
         return list;
     }
