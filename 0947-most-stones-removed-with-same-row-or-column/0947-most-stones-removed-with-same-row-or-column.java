c3479f7fe4c8823a70c8352aec2ec2e04d7dc4bc
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-19 04:11:13 -0400

    Time: 6 ms (96.79%), Space: 44.2 MB (49.43%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,34 +1,39 @@
 class Solution {
+    int ans;
     public int removeStones(int[][] stones) {
-        boolean[] visited = new boolean[stones.length];
-        HashMap<Integer, List<Integer>> row = new HashMap<>();
-        HashMap<Integer, List<Integer>> col = new HashMap<>();
-        for (int i = 0; i < stones.length; i++){
-            row.computeIfAbsent(stones[i][0], o -> new ArrayList<>()).add(i); // so we can find other stones based on row
-            col.computeIfAbsent(stones[i][1], o -> new ArrayList<>()).add(i); // so we can find other stones based on col
-        }
+        int len = stones.length;
+        ans = len;
+        int[] parent = new int[len];
+        for (int i = 0; i < len; i++) parent[i] = i;
+        ans -= helper(0, stones,parent, len);
+        ans -= helper(1, stones,parent, len);
+        return len - ans;
+    }
 
-        int numOfIsland = 0;
-        for (int i = 0; i < stones.length; i++)
-            if (!visited[i]){
-                removeIsland(visited, stones, i, row, col);
-                numOfIsland++;
+    public int find(int[] parent, int curindex){
+        int temp = curindex;
+        while (curindex != parent[curindex]){
+            curindex = parent[curindex];
+        }parent[temp] = curindex;
+        return curindex;
+    }
+
+    public int helper(int num, int[][] stones, int[] parent, int len){
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <len; i++){
+            int[] curCoor = stones[i];
+            if (map.containsKey(curCoor[num])){
+                int pindex = find(parent,map.get(curCoor[num]));
+                int cindex = find(parent, i);
+                if (pindex != cindex){
+                    parent[cindex] = parent[pindex];
+                    ans ++;
+                }
+            }else{
+                map.put(curCoor[num], i);
             }
-
-        return stones.length - numOfIsland;
+        }return ans;
     }
 
-    private void removeIsland(boolean[] visited, int[][] stones, int idx,
-            Map<Integer, List<Integer>> row, Map<Integer, List<Integer>> col){
-        if (visited[idx]) return; // base case
-
-        visited[idx] = true; // mark as visited.
-        int r = stones[idx][0];
-        int c = stones[idx][1];
-        for (int index : row.get(r)) // not null because the current stone is in it
-            removeIsland(visited, stones, index, row, col); // visit all the stones on this row
-
-        for (int index : col.get(c))
-            removeIsland(visited, stones, index, row, col); // visit all the stones on this col
-    }
 }
