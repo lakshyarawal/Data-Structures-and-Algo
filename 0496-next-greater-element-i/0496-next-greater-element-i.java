f1396d70dcc0c27255c2f34c054cb8c9134b4c4f
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-21 06:31:24 -0400

    Time: 4 ms (80.10%), Space: 43.7 MB (54.82%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> valueMap = new HashMap<>();
-        for(int i = 0; i < nums2.length; i++){
-            valueMap.put(nums2[i], i);
-        }
-        int[] res = new int[nums1.length];
-        Arrays.fill(res, -1);
-        for(int j = 0; j < nums1.length; j++){
-            for(int i = valueMap.get(nums1[j])+1; i < nums2.length; i++){
-                if(nums2[i] > nums1[j]){
-                    res[j] = nums2[i];
-                    break;
-                }
-                res[j] = -1;
-            }
-        }
-        return res;
+       int ans[]=new int[nums2.length];
+       int finalans[]=new int[nums1.length];
+       HashMap<Integer , Integer> map=new HashMap<>();
+       Stack <Integer> s=new Stack<>();
+       for(int i=nums2.length-1; i>=0; i--){
+           while(!s.isEmpty() && nums2[s.peek()]<nums2[i]){
+               s.pop();
+           }
+
+           if(s.isEmpty()){
+               ans[i]=-1;
+           }else{
+               ans[i]=nums2[s.peek()];
+           }
+           s.push(i);
+       }
+
+       for(int i=0; i<nums2.length; i++){
+           map.put(nums2[i] , ans[i]);
+       }
+       for(int i=0; i<nums1.length; i++){
+          int t= map.get(nums1[i]);
+          finalans[i]=t;
+       }
+
+       return finalans;
     }
 }
