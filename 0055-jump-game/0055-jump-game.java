d9d871489ae305b9920a1f9b25eaebbc3a0036b8
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-25 06:29:20 -0400

    Time: 1 ms (99.99%), Space: 44.1 MB (81.66%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,26 +1,11 @@
-
-
 class Solution {
-
-    
     public boolean canJump(int[] nums) {
         int lastIndex = nums.length-1;
-        
-        for(int i = nums.length-2; i>= 0; i --){
-            if(i + nums[i] >= lastIndex){
+        for(int i = nums.length-2; i>= 0; i--){
+            if(i + nums[i] >= lastIndex)
                 lastIndex = i;
             }
-            
-            }
-            
-        
-        
-        if(lastIndex == 0){
-            return true;
-        }
-        
+        if(lastIndex == 0) return true;
         return false;
-        
     }
-    
 }
