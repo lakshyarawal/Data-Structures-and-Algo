4a7e9ca386cae5f0e5c4ed611980d559ab643dc6
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-25 06:17:31 -0400

    Time: 28 ms (98.09%), Space: 46.9 MB (80.04%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -38,40 +38,45 @@ class Solution {
     
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        //hashMap to store all parents of emails
+        HashMap<String, Integer> parentMap = new HashMap<>();
+        //Disjoint set to keep track of common parents
         DisjointSet ds = new DisjointSet(n);
+        
+        //For each Email add it to map and union the parents if there are any common
         for(int i = 0; i< accounts.size();i++){
+            
+            List<String> currentAccountEmails = accounts.get(i);
+            
             for(int j = 1; j< accounts.get(i).size(); j++){
-                if(hashMap.containsKey(accounts.get(i).get(j))){
-                    ds.unionByRank(hashMap.get(accounts.get(i).get(j)), i);
-                }
-                else{
-                    hashMap.put(accounts.get(i).get(j), i);
-                }
+                
+                String email = currentAccountEmails.get(j);   
+                if(parentMap.containsKey(email)) ds.unionByRank(parentMap.get(email), i);
+                else parentMap.put(email, i);
             }
         }
-
+        
+        //Create an array for each accounts index
         ArrayList<String> res[] = new ArrayList[n];
-        for(int i = 0; i< n; i++){
+        for(int i = 0; i < n; i++){
             res[i] = new ArrayList<>();
         }
-        for(Map.Entry<String, Integer> e: hashMap.entrySet()){
+        // For each account add the emails as result of union find
+        for(Map.Entry<String, Integer> e: parentMap.entrySet()){
             int uTP = ds.getParent(e.getValue());
             res[uTP].add(e.getKey());
         }
 
         List<List<String>> list = new ArrayList<>();
         
-        for(int i = 0; i< n; i++){
+        //Sort the lists that are not empty and add to it to final result
+        for(int i = 0; i < n; i++){
             if(res[i].size() == 0){
                 continue;
             }
-            List<String> res2 = new ArrayList<>();
             Collections.sort(res[i]);
-            res2.add(accounts.get(i).get(0));
-            for(String E: res[i])
-                res2.add(E);
-            list.add(res2);
+            res[i].add(0, accounts.get(i).get(0));
+            list.add(res[i]);
         }
         return list;
 
