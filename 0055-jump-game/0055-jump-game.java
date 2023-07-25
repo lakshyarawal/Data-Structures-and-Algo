48bd1699141a6a98a033a6d641d9d84421f49b10
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-25 06:28:03 -0400

    Time: 1 ms (99.99%), Space: 44.4 MB (66.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,26 @@
+
+
 class Solution {
+
+    
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return true;
-        int[] dp = new int[n];
-        dp[0] = 1;
-        for(int i = 0; i < n; i++){
-            if(dp[i] != 1) continue;
-            for(int j = i; j <= i + nums[i] && j < n; j++){
-                if(j == n-1) return true;
-                dp[j] = 1;
+        int lastIndex = nums.length-1;
+        
+        for(int i = nums.length-2; i>= 0; i --){
+            if(i + nums[i] >= lastIndex){
+                lastIndex = i;
             }
+            
+            }
+            
+        
+        
+        if(lastIndex == 0){
+            return true;
         }
-        if(dp[n-1] == 0) return false;
-        return true;
+        
+        return false;
+        
     }
+    
 }
