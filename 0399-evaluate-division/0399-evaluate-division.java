76f57f232c06cb7a2ecd935e47316307426bc646
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-26 08:57:19 -0400

    Time: 1 ms (93.79%), Space: 41 MB (84.71%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,51 +1,75 @@
 class Solution {
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String, HashMap<String, Double>> edges = new HashMap<>();
-        for(int i=0; i<equations.size(); i++){
-            String from = equations.get(i).get(0);
-            String to = equations.get(i).get(1);
-            //a to b
-            if(edges.containsKey(from) == false){
-                edges.put(from, new HashMap<>());
-            }
-            edges.get(from).put(to, values[i]);
-            //b to a
-            if(edges.containsKey(to) == false){
-                edges.put(to, new HashMap<>());
-            }
-            edges.get(to).put(from, 1/values[i]);
+    private static class Edge {
+        String vertex;
+        double weight;
+        
+        public Edge(String vertex, double weight) {
+            this.vertex = vertex;
+            this.weight = weight;
         }
-
-        //now standard dfs for queries[i][0] to find queries[i][1]
-        int n = queries.size();
-        double[] output = new double[n];
-        for(int i=0; i<n; i++){
-            double[] tmp = new double[1];
-            tmp[0] = -1;
-            String from = queries.get(i).get(0);
-            String to = queries.get(i).get(1);
-            dfs(new HashSet<String>(), from, 1, tmp, to, edges);
-            output[i] = tmp[0];
-        }
-        return output;
     }
 
-    private void dfs(HashSet<String> visited, String cur, double val, double[] tmp, String target, HashMap<String, HashMap<String, Double>> map){
-        if(visited.contains(cur)){
-            return;
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Edge>> graph = buildGraph(equations, values);
+        double[] results = new double[queries.size()];
+        
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> query = queries.get(i);
+            String start = query.get(0);
+            String end = query.get(1);
+            
+            if (!graph.containsKey(start) || !graph.containsKey(end)) {
+                results[i] = -1.0;
+            } else {
+                Set<String> visited = new HashSet<>();
+                double result = dfs(graph, start, end, visited);
+                results[i] = result;
+            }
         }
-        visited.add(cur);
-        if(cur.equals(target) && map.containsKey(cur)){
-            tmp[0] = val;
-            return;
+        
+        return results;
+    }
+
+    private Map<String, List<Edge>> buildGraph(List<List<String>> equations, double[] values) {
+        Map<String, List<Edge>> graph = new HashMap<>();
+        
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String start = equation.get(0);
+            String end = equation.get(1);
+            double value = values[i];
+            
+            // Add edges in both directions with corresponding weights
+            graph.putIfAbsent(start, new ArrayList<>());
+            graph.get(start).add(new Edge(end, value));
+            
+            graph.putIfAbsent(end, new ArrayList<>());
+            graph.get(end).add(new Edge(start, 1.0 / value));
         }
-        HashMap<String, Double> curMap = map.get(cur);
-        if(curMap == null){
-            return;
+        
+        return graph;
+    }
+
+    private double dfs(Map<String, List<Edge>> graph, String curr, String target, Set<String> visited) {
+        if (curr.equals(target)) {
+            return 1.0;
         }
-        for(String neighbour : curMap.keySet()){
-            dfs(visited, neighbour, val*curMap.get(neighbour), tmp, target, map);
+        
+        visited.add(curr);
+        List<Edge> neighbors = graph.get(curr);
+        
+        for (Edge neighbor : neighbors) {
+            if (!visited.contains(neighbor.vertex)) {
+                double result = dfs(graph, neighbor.vertex, target, visited);
+                
+                if (result != -1.0) {
+                    // Found a valid path, return the product of the current value and result
+                    return neighbor.weight * result;
+                }
+            }
         }
-        return;
+        
+        // No valid path found
+        return -1.0;
     }
 }
