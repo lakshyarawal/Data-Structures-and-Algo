78055645d2c923e1453b0e9fd90f738838450ccf
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-27 06:21:42 -0400

    Time: 39 ms (85.82%), Space: 56.4 MB (31.72%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
@@ -1,24 +1,26 @@
 class Solution {
+
     public int minimumSize(int[] nums, int maxOperations) {
-        int left = 1;
-        int right = 1_000_000_000;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (operations(nums, mid) > maxOperations)
-                left = mid + 1;
-            else
-                right = mid;
+        int lo = 1, hi = 1_000_000_001;
+        while (lo < hi) {
+            int size = lo + hi >> 1;
+            if (canDivide(nums, size, maxOperations)) {
+                hi = size;
+            }else {
+                lo = size + 1;
+            }
         }
-        return left;
+        return lo;
     }
-
-    private int operations(int[] nums, int mid) {
-        int operations = 0;
-        for (int num : nums) {
-            operations += (num - 1) / mid;
+    private boolean canDivide(int[] nums, int size, int maxOperations) {
+        int cnt = 0;
+        for (int balls : nums) {
+         // cnt += balls / size - (balls % size == 0 ? 1 : 0);
+            cnt += (balls - 1) / size;		 
+            if (cnt > maxOperations) {
+                return false;
+            }
         }
-
-        return operations;
+        return true;
     }
 }
