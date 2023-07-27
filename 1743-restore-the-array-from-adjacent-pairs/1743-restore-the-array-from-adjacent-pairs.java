0153b873c7f1aad18d5271fc6d1abd0c4b53c3b5
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-27 07:36:04 -0400

    Time: 66 ms (99.53%), Space: 96.9 MB (54.25%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] restoreArray(int[][] adjacentPairs) {
         Map<Integer, List<Integer>> neighbors = new HashMap<>();
+        
         for (int[] pair : adjacentPairs) {
             neighbors.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
             neighbors.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
         }
 
         int[] result = new int[adjacentPairs.length + 1];
-        Set<Integer> visited = new HashSet<>();
         int start = 0;
 
         for (Map.Entry<Integer, List<Integer>> entry : neighbors.entrySet()) {
@@ -19,12 +19,13 @@ class Solution {
 
         int index = 0;
         int prev = start;
+        int prev2 = Integer.MIN_VALUE;
 
         while (index < result.length) {
             result[index++] = prev;
-            visited.add(prev);
             for (int neighbor : neighbors.get(prev)) {
-                if (!visited.contains(neighbor)) {
+                if (neighbor != prev2) {
+                    prev2 = prev;
                     prev = neighbor;
                     break;
                 }
