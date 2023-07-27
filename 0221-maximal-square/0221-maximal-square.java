02871f4ec48a95fa400c759576c7e8529aebc735
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-27 09:00:34 -0400

    Time: 4 ms (99.88%), Space: 54.3 MB (43.12%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int res = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j]=='1'){
-                    matrix[i][j] = (char)(1 + min(
-                        i>0 ? (int)matrix[i-1][j] : 0,
-                        j>0 ? (int)matrix[i][j-1] : 0,
-                        i>0 && j>0 ? (int)matrix[i-1][j-1] : 0
-                    ));
-                    
-                    res = Math.max(res, (int)matrix[i][j]);
-                } else {
-                    matrix[i][j] = (char)0;
+        if(matrix.length==0 || matrix[0].length==0)
+            return 0;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] dp=new int [n+1][m+1];
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+                dp[i][j]=0;
+        int maxsize=0;
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                    dp[i][j]=Math.min( dp[i-1][j], Math.min ( dp[i-1][j-1], dp[i][j-1] )) + 1 ;
+                        maxsize=Math.max(maxsize,dp[i][j]);
                 }
             }
-        }
-        return res * res;
-    }
-    
-    int min(int i, int j, int k){
-        return Math.min(i, Math.min(j, k));
+        return maxsize*maxsize;
     }
 }
