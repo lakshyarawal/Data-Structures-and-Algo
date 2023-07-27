c90ebe0015b74bdd82e77abbf309cba5663605c2
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-27 09:01:43 -0400

    Time: 4 ms (99.88%), Space: 54.4 MB (43.12%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -5,7 +5,9 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         int[][] dp=new int [n+1][m+1];
-
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+                dp[i][j]=0;
         int maxsize=0;
         for(int i=1;i<=n;i++)
             for(int j=1;j<=m;j++)
@@ -13,7 +15,7 @@ class Solution {
                 if(matrix[i-1][j-1]=='1')
                 {
                     dp[i][j]=Math.min( dp[i-1][j], Math.min ( dp[i-1][j-1], dp[i][j-1] )) + 1 ;
-                    maxsize=Math.max(maxsize,dp[i][j]);
+                        maxsize=Math.max(maxsize,dp[i][j]);
                 }
             }
         return maxsize*maxsize;
