5fcdb2900e6d46486e48137bcec95c9a85a399fa
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-28 14:57:21 -0400

    Time: 0 ms (100.00%), Space: 40.3 MB (91.43%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,39 +1,31 @@
-public class Solution {
+class Solution {
     public int longestSubstring(String s, int k) {
-        char[] str = s.toCharArray();
-        int[] counts = new int[26];
-        int h, i, j, idx, max = 0, unique, noLessThanK;
+        if (s == null || s.length() == 0) return 0;
+        if (k<2) return s.length();
+        return helper(s, 0, s.length(), k);
+    }
+
+    public int helper(String s, int l, int r, int k) {
+        if (l>=r) return 0;
         
-        for (h = 1; h <= 26; h++) {
-            Arrays.fill(counts, 0);
-            i = 0; 
-            j = 0;
-            unique = 0;
-            noLessThanK = 0;
-            while (j < str.length) {
-                if (unique <= h) {
-                    idx = str[j] - 'a';
-                    if (counts[idx] == 0)
-                        unique++;
-                    counts[idx]++;
-                    if (counts[idx] == k)
-                        noLessThanK++;
-                    j++;
-                }
-                else {
-                    idx = str[i] - 'a';
-                    if (counts[idx] == k)
-                        noLessThanK--;
-                    counts[idx]--;
-                    if (counts[idx] == 0)
-                        unique--;
-                    i++;
-                }
-                if (unique == h && unique == noLessThanK)
-                    max = Math.max(j - i, max);
+        // build freq map
+        int[] freq = new int[26];
+        for (int i=l; i<r; i++) freq[s.charAt(i)-'a']++;
+        
+        // check if valid
+        boolean valid = true;
+        for (int i=0; i<26 && valid; i++) if (freq[i] > 0 && freq[i] < k) valid = false;
+        if (valid) return r-l;
+        
+        // if not for each invalid character start a new split search
+        int best = 0, start=l;
+        for (int i=l; i<r; i++) {
+            if (freq[s.charAt(i) -'a'] < k) {
+                best = Math.max(best, helper(s, start, i, k));
+                start = i+1;
             }
         }
-        
-        return max;
+        best = Math.max(best, helper(s, start, r, k));
+        return best;
     }
 }
