2c5a6265f4f0fb50b5be693e765c3959258beed6
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-29 03:52:39 -0400

    Time: 1 ms (79.52%), Space: 40.4 MB (91.10%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,31 +1,24 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        if (s == null || s.length() == 0) return 0;
-        if (k<2) return s.length();
-        return helper(s, 0, s.length(), k);
-    }
-
-    public int helper(String s, int l, int r, int k) {
-        if (l>=r) return 0;
+        int n = s.length();
+        if(n == 0 || n < k) return 0;
+        if(k < 2) return n;
         
-        // build freq map
-        int[] freq = new int[26];
-        for (int i=l; i<r; i++) freq[s.charAt(i)-'a']++;
-        
-        // check if valid
-        boolean valid = true;
-        for (int i=0; i<26 && valid; i++) if (freq[i] > 0 && freq[i] < k) valid = false;
-        if (valid) return r-l;
-        
-        // if not for each invalid character start a new split search
-        int best = 0, start=l;
-        for (int i=l; i<r; i++) {
-            if (freq[s.charAt(i) -'a'] < k) {
-                best = Math.max(best, helper(s, start, i, k));
-                start = i+1;
-            }
+        Map<Character, Integer> counts = new HashMap<>();
+        for(char c : s.toCharArray()){
+            counts.put(c, counts.getOrDefault(c, 0)+1);
         }
-        best = Math.max(best, helper(s, start, r, k));
-        return best;
+        int l = 0;
+        while(l < n && counts.get(s.charAt(l)) >= k) l++;
+        if(l >= n-1) return l;
+        
+        int ls1 = longestSubstring(s.substring(0, l), k);
+        
+        while(l < n && counts.get(s.charAt(l)) < k) l++;
+        
+        int ls2 = (l < n) ? longestSubstring(s.substring(l), k) : 0;
+        return Math.max(ls1, ls2);
+        
+        
     }
 }
