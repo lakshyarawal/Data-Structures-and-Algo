d6cb6da005b37a0f4c96eb9f0d7a499bcca571a3
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-29 16:48:03 -0400

    Time: 14 ms (88.32%), Space: 51 MB (66.29%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,24 +1,31 @@
 class Solution {
- public boolean possibleBipartition(int N, int[][] dislikes) {
-        boolean[][] g = new boolean[N][N];
-        for (int[] d : dislikes) {
-            g[d[0] - 1][d[1] - 1] = true;
-            g[d[1] - 1][d[0] - 1] = true;
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        int[] color = new int[N + 1];
+        List<List<Integer>> adj = new ArrayList<>(N + 1);
+        for(int i = 0; i <= N; i++) adj.add(new ArrayList<Integer>());
+        for(int[] d : dislikes) {
+            adj.get(d[0]).add(d[1]);
+            adj.get(d[1]).add(d[0]);
         }
-        int[] colors = new int[N];
-        for (int i = 0; i < N; i++)
-            if (colors[i] == 0 && !paint(i, 1, colors, g))
-                return false;
-        return true;
-    }
-
-    boolean paint(int u, int color, int[] colors, boolean[][] g) {
-        if (colors[u] != 0)
-            return colors[u] == color;
-        colors[u] = color;
-        for (int v = 0; v < colors.length; v++)
-            if (g[u][v] && !paint(v, -color, colors, g))
-                return false;
+        
+        for(int i = 1; i <= N; i++) {
+            if(color[i] == 0) {
+                color[i] = 1;
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                while(!q.isEmpty()) {
+                    int cur = q.poll();
+                    for(int nb : adj.get(cur)) {
+                        if(color[nb] == 0) {
+                            color[nb] = color[cur] == 1 ? 2 : 1;
+                            q.add(nb);
+                        } else {
+                            if(color[nb] == color[cur]) return false;
+                        }
+                    }
+                }
+            }
+        }
         return true;
     }
 }
