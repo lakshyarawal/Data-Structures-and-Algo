96dd84d7e35fcab05ede5c6b7417cc54a69ffbaa
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-07-30 10:04:11 -0400

    Time: 11 ms (94.88%), Space: 44.6 MB (55.49%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,37 +1,30 @@
-public class Solution {
-    public String removeKdigits(String num, int k) {
-        int len = num.length();
-        //corner case
-        if(k==len)        
-            return "0";
-            
-        Stack<Character> stack = new Stack<>();
-        int i =0;
-        while(i<num.length()){
-            //whenever meet a digit which is less than the previous digit, discard the previous one
-            while(k>0 && !stack.isEmpty() && stack.peek()>num.charAt(i)){
-                stack.pop();
+class Solution {
+    public String removeKdigits(String num, int k) {        
+        if(k >= num.length()) return "0";
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : num.toCharArray()) {
+            while(k > 0 && !stack.isEmpty() && stack.peekLast() > c) {
+                stack.removeLast();
                 k--;
             }
-            stack.push(num.charAt(i));
-            i++;
+            stack.addLast(c);
         }
         
-        // corner case like "1111"
-        while(k>0){
-            stack.pop();
-            k--;            
+        while(k>0) {
+            stack.removeLast();
+            k--;
         }
         
-        //construct the number from the stack
+        // Remove all zeros from the front of the stack and then if stack is empty, return "0"
+        while(!stack.isEmpty() && stack.peekFirst()== '0') stack.removeFirst();
+        if(stack.isEmpty()) return "0";
+
+        // build the number from the stack
         StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty())
-            sb.append(stack.pop());
-        sb.reverse();
-        
-        //remove all the 0 at the head
-        while(sb.length()>1 && sb.charAt(0)=='0')
-            sb.deleteCharAt(0);
+        while(!stack.isEmpty()) {
+            sb.append(stack.removeFirst());
+        }
         return sb.toString();
     }
 }
