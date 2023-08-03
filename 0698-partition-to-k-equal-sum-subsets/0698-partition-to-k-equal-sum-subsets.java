f2f3dabf3349e88a3c15310bfb33cf045f88e1d7
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-03 18:53:01 +0530

    Time: 1631 ms (34.89%), Space: 40.3 MB (60.97%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,27 +1,29 @@
 class Solution {
+    public int subsetTargetSum;
   public boolean canPartitionKSubsets(int[] nums, int k) {
     final int sum = Arrays.stream(nums).sum();
     if (sum % k != 0)
       return false;
 
     final int t = sum / k; // Each subset's target sum
+      subsetTargetSum = t;
     boolean[] seen = new boolean[nums.length];
-    return dfs(nums, 0, k, t, t, seen);
+    return dfs(nums, 0, k, t, seen);
   }
 
-  private boolean dfs(int[] nums, int s, int k, int target, int subsetTargetSum, boolean[] seen) {
+  private boolean dfs(int[] nums, int s, int k, int target, boolean[] seen) {
     if (k == 0)
       return true;
     if (target < 0)
       return false;
     if (target == 0)
-      return dfs(nums, 0, k - 1, subsetTargetSum, subsetTargetSum, seen);
+      return dfs(nums, 0, k - 1, subsetTargetSum, seen);
 
     for (int i = s; i < nums.length; ++i) {
       if (seen[i])
         continue;
       seen[i] = true;
-      if (dfs(nums, i + 1, k, target - nums[i], subsetTargetSum, seen))
+      if (dfs(nums, i + 1, k, target - nums[i], seen))
         return true;
       seen[i] = false;
     }
