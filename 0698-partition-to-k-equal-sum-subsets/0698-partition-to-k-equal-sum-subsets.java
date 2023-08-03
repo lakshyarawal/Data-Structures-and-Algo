c258df4d40c6e37bd39bdeba1950cefb6985b184
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-03 18:47:32 +0530

    Time: 992 ms (42.75%), Space: 39.9 MB (92.07%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,42 +1,29 @@
 class Solution {
+int sum;
+public boolean solve(int nums[],int target,int index,int[] sums){
+    if(index==nums.length){
+        for(int i=1;i<sums.length;i++){
+            if(sums[i] != sums[i-1]) return false;
+        }
+        return true;
+    }
+    for(int j=0;j<sums.length;j++){
+        if(sums[j]+nums[index]<=target){
+            sums[j]+=nums[index];
+            if(solve(nums,target,index+1,sums)) return true;
+            sums[j]-=nums[index];
+        }
+        if(sums[j]==0) return false;
+    }
+    return false;
+}
 public boolean canPartitionKSubsets(int[] nums, int k) {
-		if(nums == null || nums.length == 0)
-			return false;
-		
-		int n = nums.length;
-		//result array
-		boolean dp[] = new boolean[1<<n];
-		int total[] = new int[1<<n];
-		dp[0] = true;
-		
-		int sum = 0;
-		for(int num : nums)
-			sum += num;
-		Arrays.sort(nums);
-		
-		if(sum%k != 0) 
-			return false;
-		sum /= k;
-		if(nums[n-1] > sum)
-			return false;
-		// Loop over power set
-		for(int i = 0;i < (1<<n);i++) {
-			if(dp[i]) {
-				// Loop over each element to find subset
-				for(int j = 0;j < n;j++) {
-					// set the jth bit 
-					int temp = i | (1 << j);
-					if(temp != i) {
-						// if total sum is less than target store in dp and total array
-						if(nums[j] <= (sum- (total[i]%sum))) {
-							dp[temp] = true;
-							total[temp] = nums[j] + total[i];
-						} else
-							break;
-					}
-				}
-			}
-		}
-		return dp[(1<<n) - 1];
-	}
+    sum=0;
+    for(int num:nums){
+        sum+=num;
+    }
+    if(sum%k!=0 || nums.length<k) return false;
+    Arrays.sort(nums);
+    return solve(nums,sum/k,0,new int[k]);
+}
 }
