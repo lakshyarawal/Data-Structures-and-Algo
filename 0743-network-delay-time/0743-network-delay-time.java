d2dd608cba9681194446544961bae8e6b7d299bd
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-03 14:22:07 +0530

    Time: 14 ms (73.31%), Space: 48.2 MB (15.90%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,29 +1,29 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<List<Pair<Integer, Integer>>> adj = new ArrayList<>(n);
-        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        List<List<Pair<Integer, Integer>>> adj = new ArrayList<>(n+1);
+        for(int i = 0; i <= n; i++) adj.add(new ArrayList<>());
         for(int[] time : times){
-            adj.get(time[0]-1).add(new Pair(time[1], time[2]));
+            adj.get(time[0]).add(new Pair(time[1], time[2]));
         }
         Queue<Integer> q = new LinkedList<>();
         q.offer(k);
-        int[] minTime = new int[n];
+        int[] minTime = new int[n+1];
         Arrays.fill(minTime, Integer.MAX_VALUE);
-        minTime[k-1] = 0;
+        minTime[k] = 0;
         while(!q.isEmpty()){
             int curNode = q.remove();
-            int stepTime = minTime[curNode-1];
+            int stepTime = minTime[curNode];
 
-            for(Pair<Integer, Integer> nNode : adj.get(curNode-1)){
-                if(stepTime+nNode.getValue() < minTime[nNode.getKey() - 1]){
-                    minTime[nNode.getKey() - 1] = stepTime+nNode.getValue();
+            for(Pair<Integer, Integer> nNode : adj.get(curNode)){
+                if(stepTime+nNode.getValue() < minTime[nNode.getKey()]){
+                    minTime[nNode.getKey()] = stepTime+nNode.getValue();
                     q.offer(nNode.getKey());
                 }
             }
         }
 
         int time = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i <= n; i++){
             if(minTime[i] == Integer.MAX_VALUE)return -1;
             time = Math.max(time, minTime[i]);
         }
