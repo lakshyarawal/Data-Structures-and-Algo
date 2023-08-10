5dab42ad34726a2a0a8270c83a295791d9a8d0b4
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-10 19:58:58 +0530

    Time: 16 ms (90.48%), Space: 53.6 MB (100.00%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -2,14 +2,9 @@ class Solution {
    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
-        int maxHorizontal = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
-        int maxVertical = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
-        for (int i = 1; i < horizontalCuts.length; i++) {
-            maxHorizontal = Math.max(maxHorizontal, horizontalCuts[i] - horizontalCuts[i - 1]);
-        }
-        for (int i = 1; i < verticalCuts.length; i++) {
-            maxVertical = Math.max(maxVertical, verticalCuts[i] - verticalCuts[i - 1]);
-        }
+        int maxHorizontal = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]), maxVertical = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
+        for (int i = 1; i < horizontalCuts.length; i++) maxHorizontal = Math.max(maxHorizontal, horizontalCuts[i] - horizontalCuts[i - 1]);
+        for (int i = 1; i < verticalCuts.length; i++) maxVertical = Math.max(maxVertical, verticalCuts[i] - verticalCuts[i - 1]);
         return (int) ((long) maxHorizontal * maxVertical % 1_000_000_007);
     }
 }
