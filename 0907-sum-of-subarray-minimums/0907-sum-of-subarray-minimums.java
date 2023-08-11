ecb9ff0150f651f2beb73183a9906eef247a0e73
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-11 18:05:18 +0530

    Time: 62 ms (66.80%), Space: 47.6 MB (62.30%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,22 +1,31 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int[] dis = new int[arr.length];
-        Stack<int[]> st = new Stack<>();
-        for(int i = 0; i< arr.length; i++) {
-            while(!st.empty() && st.peek()[0] >= arr[i]) {
-                st.pop();
+        int length = arr.length;
+        long mod = (long)1e9 + 7;
+        long result = 0;
+        Stack<int[]> previousLess = new Stack<>();
+        Stack<int[]> nextLess = new Stack<>();
+        int [] left = new int [length];
+        int [] right = new int [length];
+        // previous less element - imagine 2, 5, 6, 5 you don't want to double count so you can either enforce the = on the left or right array.
+        for (int i=0; i<length; i++) {
+            while (!previousLess.isEmpty() && previousLess.peek()[0] >= arr[i]) {
+                previousLess.pop();
             }
-            dis[i] = st.empty() ? i + 1: i - st.peek()[1];
-            st.push(new int[]{arr[i], i});
+            left[i] = previousLess.isEmpty() ? i + 1 : i - previousLess.peek()[1];
+            previousLess.push(new int []{arr[i], i});
         }
-        double count  = 0;
-        double[] c = new double[arr.length + 1];
-        for(int i = 0; i < arr.length; i++) {
-            c[i + 1] = c[i - dis[i] + 1] + arr[i] * (dis[i]);
+        // next less element. for this we do reverse traversal
+        for (int i=length - 1; i>=0; i--) {
+            while (!nextLess.isEmpty() && nextLess.peek()[0] > arr[i]) {
+                nextLess.pop();
+            }
+            right[i] = nextLess.isEmpty() ? length - i : nextLess.peek()[1] - i;
+            nextLess.push(new int [] {arr[i], i});
         }
-        for(double d: c) {
-            count += d;
+        for (int i=0; i<length; i++) {
+            result = (result + (long) arr[i] * left[i] * right[i]) % mod; 
         }
-        return (int)(count % 1000000007);
+        return (int)result;
     }
 }
