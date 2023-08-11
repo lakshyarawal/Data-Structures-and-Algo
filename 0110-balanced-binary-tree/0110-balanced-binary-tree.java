343a454b5faad7818afca6c646c5838b12ea0408
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-11 19:37:27 +0530

    Time: 0 ms (100.00%), Space: 43 MB (87.90%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,33 +14,19 @@
  * }
  */
 class Solution {
-     private int height(TreeNode root) {
-    // An empty tree has height -1
-        if (root == null) {
-          return -1;
-        }
-         int leftHeight = height(root.left);
-         if(leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
-         
-         int rightHeight = height(root.right);
-         if(rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
-         
-         int heightDiff = Math.abs(leftHeight - rightHeight);
-         
-         if(heightDiff > 1){
-             return Integer.MIN_VALUE;
-         }
-         
-         else{
-              return 1 + Math.max(leftHeight, rightHeight);
-         }
-       
-      }
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-          return true;
-        }
-        
-        return height(root) != Integer.MIN_VALUE;
+        if (root == null)
+            return true;
+        int depthLeft = depth(root.left);
+        int depthRight = depth(root.right);
+        if (Math.abs(depthRight - depthLeft) > 1 )
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth (TreeNode node) {
+        if (node == null)
+            return 0;
+        return Math.max(depth(node.left), depth(node.right)) + 1;
     }
 }
