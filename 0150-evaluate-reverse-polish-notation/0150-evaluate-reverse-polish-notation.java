5e7cc1eb4716058e5eb2d3a5e2a1f005cdec0ba0
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-17 13:10:24 +0530

    Time: 7 ms (69.05%), Space: 43.7 MB (29.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,39 +1,35 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        Stack<String> s = new Stack<>();
-        for(String c : tokens){
-            //System.out.println(c);
-            if(c.equals("+") || c.equals("-") || c.equals("/") || c.equals("*")){
-                int a =0, b=0;
-                if(!s.isEmpty()){
-                    a = Integer.valueOf(s.pop());
-                }
-                if(!s.isEmpty()){
-                    b = Integer.valueOf(s.pop());
-                }
-                int res = 0;
-                
-                switch (c) {
-                  case "+":
-                    res = a+b;
-                    break;
-                  case "-":
-                    res = b-a;
-                    break;
-                  case "/":
-                    res = b/a;
-                    break;
-                  case "*":
-                    res = b*a;
-                    break;
-                }
-                s.push(String.valueOf(res));
-            }
-            else{
-                s.push(c);
-            }
+    private Stack<String> s;
+    
+    private int calculate(String c){
+        int a =0, b=0, res = 0;
+        
+        if(!s.isEmpty()) a = Integer.valueOf(s.pop());
+        if(!s.isEmpty()) b = Integer.valueOf(s.pop());
+
+        switch (c) {
+          case "+":
+            res = a+b;
+            break;
+          case "-":
+            res = b-a;
+            break;
+          case "/":
+            res = b/a;
+            break;
+          case "*":
+            res = b*a;
+            break;
         }
-        //System.out.println(Arrays.toString(s.toArray()));
+        return res;  
+    }
+    
+    public int evalRPN(String[] tokens) {
+        s = new Stack<>();
+        for(String c : tokens){
+            if(c.equals("+") || c.equals("-") || c.equals("/") || c.equals("*")) s.push(String.valueOf(calculate(c)));
+            else s.push(c);
+        }
         return Integer.valueOf(s.pop());
     }
 }
