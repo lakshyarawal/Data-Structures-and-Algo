a41630a8c607cc3a410344cb0230dfc2bc4caa96
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-18 17:03:32 +0530

    Time: 1 ms (99.64%), Space: 43.2 MB (61.79%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -18,26 +18,21 @@ class Solution {
         Queue<TreeNode> q = new LinkedList<>();
         Queue<Integer> qIndex = new LinkedList<>();
         q.add(root);
-        qIndex.add(1); //store index, assuming root's index is 1
+        qIndex.add(1);
         int max = 0;
-        while(!q.isEmpty())
-        {
+        while(!q.isEmpty()){
             int size = q.size();
             int start = 0, end = 0;
-            for(int i=0; i<size; i++)
-            {
+            for(int i=0; i<size; i++){
                 TreeNode node = q.remove();
                 int index = qIndex.remove();
-                if(i==0) start = index; //start and end index for each level
+                if(i==0) start = index;
                 if(i==size-1) end = index;
-                if(node.left!=null)
-                {
+                if(node.left!=null){
                     q.add(node.left);
                     qIndex.add(2*index);
                 }
-                
-                if(node.right!=null)
-                {
+                if(node.right!=null){
                     q.add(node.right);
                     qIndex.add(2*index+1);
                 }
