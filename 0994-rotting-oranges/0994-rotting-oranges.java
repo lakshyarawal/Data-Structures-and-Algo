a829825eca830cefef41b0829e79afe9c5b94234
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-18 14:03:45 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (99.52%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,7 +1,5 @@
 class Solution {
     private int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
-    private int res;
-    
     public void dfs(int r, int c, int[][] grid, int t, int[][] time){
         for(int[] dir : dirs){
             int newr = r+dir[0];
@@ -14,18 +12,14 @@ class Solution {
             }
         }
     }
-    
     public int orangesRotting(int[][] grid) {
-        res = 0;
-        int[][] time = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                if(grid[i][j] == 2){
-                    time[i][j] = 0;
-                }
-                if(grid[i][j] == 1){
-                    time[i][j] = Integer.MAX_VALUE;
-                }
+        int m = grid.length, n = grid[0].length, res = 0;;
+        if(m == 0 || n == 0) return 0;
+        int[][] time = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2) time[i][j] = 0;
+                if(grid[i][j] == 1) time[i][j] = Integer.MAX_VALUE;
             }
         }
         for(int i = 0; i < grid.length; i++){
@@ -33,22 +27,14 @@ class Solution {
                 if(grid[i][j] == 2){
                     dfs(i, j, grid, 0, time);
                 }
-                 //System.out.println(Arrays.deepToString(time));
             }
         }
-        
-//         System.out.println(Arrays.deepToString(grid));
-        //System.out.println(Arrays.deepToString(time));
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                } else if(grid[i][j] == -1){
-                    res = Math.max(res, time[i][j]);
-                }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1) return -1;
+                else if(grid[i][j] == -1) res = Math.max(res, time[i][j]);
             }
         }
-        
         return res;
     }
 }
