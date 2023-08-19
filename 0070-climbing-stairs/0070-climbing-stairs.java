2d628f7db5e4e349343140474545a497e7dcd454
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-19 16:12:37 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (87.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 1){
-            return 1;
-        }
-        int a = 1;
-        int b = 2;
+        if(n <= 1) return 1;
+        int a = 1, b = 2;
         for(int i = 3; i <= n; i++){
             int c = a + b;
             a = b;
