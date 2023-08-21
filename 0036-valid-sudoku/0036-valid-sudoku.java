28bd29fc33e7dab7555155a564103f492c5cee4c
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-21 13:23:40 +0530

    Time: 12 ms (35.41%), Space: 44 MB (22.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,50 +1,26 @@
 class Solution {
     
-    public boolean isValidRow(char[] row){
-        Set<Character> s = new HashSet<>();
-        for(int i = 0; i < row.length; i++){
-            if(row[i] == '.') continue;
-            if(s.contains(row[i])) return false;
-            s.add(row[i]);
-        }
-        return true;
-    }
-
-    public boolean isValidSquare(int r, int c, char[][] board){
-        Set<Character> s = new HashSet<>();
-        for(int i = r; i < r+3; i++){
-            for(int j = c; j < c+3; j++){
-                if(board[i][j] == '.') continue;
-                if(s.contains(board[i][j])) return false;
-                s.add(board[i][j]);
-            }
-        }
-        return true;
-    }
-    
-    public boolean isValidSudoku(char[][] board) {
+   public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
         
-        for(char[] row : board){
-            if(!isValidRow(row)) return false;
-        }
-        
-        char[][] cols = new char[board[0].length][board.length];
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0 ; j < board[i].length; j++){
-                cols[j][i] = board[i][j];
-            }
-        }
-        for(char[] col : cols){
-            if(!isValidRow(col)) return false;
-        }
-        
-        for(int i = 0; i < board.length; i+=3){
-            for(int j = 0 ; j < board[i].length; j+=3){
-                if(!isValidSquare(i, j, board)) return false;
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                char val = board[row][col];
+                if (val != '.') {
+                    int block = (row / 3 * 3) + (col / 3);
+                    if (set.contains("r" + row + val) || 
+                        set.contains("c" + col + val) ||
+                        set.contains("b" + block + val))
+                        return false;
+                    else {
+                        set.add("r" + row + val);
+                        set.add("c" + col + val);
+                        set.add("b" + block + val);
+                    }   
+                }
             }
         }
         
         return true;
-        
     }
 }
