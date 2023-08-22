fe328ec66729490db3e64b1874e2f17a9a354f35
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-22 16:51:34 +0530

    Time: 2 ms (87.53%), Space: 41 MB (9.70%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,16 +5,10 @@ class Solution {
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
-        
-        for(int i = 0 ; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '(' || c == '[' || c == '{'){
-                stack.push(c);
-            } else{
-                if(stack.isEmpty()) return false;
-                char top = stack.pop();
-                if(top != map.get(c)) return false;
-            }
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{') stack.push(c);
+            else if(stack.isEmpty()) return false;
+            else if(stack.pop() != map.get(c)) return false;
         }
         return stack.isEmpty();
     }
