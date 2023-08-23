3914642a1bbb2aac17f2129866c227ea211215dc
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-23 13:24:02 +0530

    Time: 8 ms (96.88%), Space: 60.7 MB (6.57%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        PriorityQueue<int[]> smallestTemperature = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int size = temperatures.length;
-        int[] res = new int[size];
-        smallestTemperature.offer(new int[]{0, temperatures[0]});
-        for(int iterator = 1; iterator < size; iterator++){
-            while(!smallestTemperature.isEmpty()  && smallestTemperature.peek()[1] < temperatures[iterator]){
-                res[smallestTemperature.peek()[0]] = iterator - smallestTemperature.peek()[0];
-                smallestTemperature.remove();
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
             }
-            smallestTemperature.offer(new int[]{iterator, temperatures[iterator]});
+            stack[++top] = i;
         }
-        return res;
+        return ret;
     }
 }
