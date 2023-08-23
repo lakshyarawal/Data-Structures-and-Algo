cd9589702170b98b1fd8acc60b3558430eeb69a3
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-23 14:28:23 +0530

    Time: 65 ms (81.32%), Space: 58.3 MB (19.41%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,5 +1,4 @@
 class Solution {
-    
     class Car implements Comparable<Car>{
         int position;
         int speed;
@@ -14,22 +13,15 @@ class Solution {
             return this.position - c.position;
         }
     }
-    
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
+        if(n == 1 || n == 0) return n;
         List<Car> allCars = new ArrayList<>();
-
-        for(int i = 0; i < n; i ++){
-            double time = (double)(target - position[i])/speed[i];
-            //System.out.println("ADDING CAR: AT POSITION: "+ position[i]+ " SPEED: "+ speed[i] + " TIME: "+ time);
-            allCars.add(new Car(position[i], speed[i], time));
-        }
+        for(int i = 0; i < n; i ++) allCars.add(new Car(position[i], speed[i], (double)(target - position[i])/speed[i]));
         Collections.sort(allCars);
         int totalFleets = 1;
-        double curFleetTime = allCars.get(allCars.size()-1).time;
-        //System.out.println("INITIAL FLEET TIME: "+ curFleetTime);
+        double curFleetTime = allCars.get(n-1).time;
         for(int i = allCars.size()-2; i >= 0; i--){
-            //System.out.println("FOR CAR AT: "+allCars.get(i).position+" FLEET TIME: "+ allCars.get(i).time);
             if(allCars.get(i).time > curFleetTime){
                 curFleetTime = allCars.get(i).time;
                 totalFleets++;
