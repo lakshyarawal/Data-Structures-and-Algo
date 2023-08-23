115aabaf5f1e88fbec1deea2aff98119121d4439
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-23 14:25:15 +0530

    Time: 68 ms (80.06%), Space: 54.2 MB (99.24%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,24 +1,40 @@
 class Solution {
-
-    public int carFleet(int target, int[] position, int[] speed) {
-        if (position.length == 1) return 1;
-        Stack<Double> stack = new Stack<>();
-        int[][] combine = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
-            combine[i][0] = position[i];
-            combine[i][1] = speed[i];
+    
+    class Car implements Comparable<Car>{
+        int position;
+        int speed;
+        double time;
+        public Car(int p, int s, double t){
+            this.position = p;
+            this.speed = s;
+            this.time = t;
         }
+        @Override
+        public int compareTo(Car c){
+            return this.position - c.position;
+        }
+    }
+    
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        List<Car> allCars = new ArrayList<>();
 
-        Arrays.sort(combine, java.util.Comparator.comparingInt(o -> o[0]));
-        for (int i = combine.length - 1; i >= 0; i--) {
-            double currentTime = (double) (target - combine[i][0]) /
-            combine[i][1];
-            if (!stack.isEmpty() && currentTime <= stack.peek()) {
-                continue;
-            } else {
-                stack.push(currentTime);
+        for(int i = 0; i < n; i ++){
+            double time = (double)(target - position[i])/speed[i];
+            //System.out.println("ADDING CAR: AT POSITION: "+ position[i]+ " SPEED: "+ speed[i] + " TIME: "+ time);
+            allCars.add(new Car(position[i], speed[i], time));
+        }
+        Collections.sort(allCars);
+        int totalFleets = 1;
+        double curFleetTime = allCars.get(allCars.size()-1).time;
+        //System.out.println("INITIAL FLEET TIME: "+ curFleetTime);
+        for(int i = allCars.size()-2; i >= 0; i--){
+            //System.out.println("FOR CAR AT: "+allCars.get(i).position+" FLEET TIME: "+ allCars.get(i).time);
+            if(allCars.get(i).time > curFleetTime){
+                curFleetTime = allCars.get(i).time;
+                totalFleets++;
             }
         }
-        return stack.size();
+        return totalFleets;
     }
 }
