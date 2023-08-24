7587071a5077f377703ee3434773d86909f52de4
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-08-24 14:27:32 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (87.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,52 @@
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int m = arr1.length;
-        int n = arr2.length;
-        if(m>n){
-            return findMedianSortedArrays(arr2,arr1);
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        int low=0,high=m,medianPos=((m+n)+1)/2;
-        while(low<=high) {
-            int cut1 = (low+high)>>1;
-            int cut2 = medianPos - cut1;
 
-            int l1 = (cut1 == 0)? Integer.MIN_VALUE:arr1[cut1-1];
-            int l2 = (cut2 == 0)? Integer.MIN_VALUE:arr2[cut2-1];
-            int r1 = (cut1 == m)? Integer.MAX_VALUE:arr1[cut1];
-            int r2 = (cut2 == n)? Integer.MAX_VALUE:arr2[cut2];
+        int total = m + n;
+        int half = (total + 1) / 2;
 
-            if(l1<=r2 && l2<=r1) {
-                if((m+n)%2 != 0)
-                    return Math.max(l1,l2);
-                else 
-                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        int left = 0;
+        int right = m;
+
+        var result = 0.0;
+
+        while (left <= right) {
+            int i = left + (right - left) / 2;
+            int j = half - i;
+
+            // get the four points around possible median
+            int left1 = (i > 0) ? nums1[i - 1] : Integer.MIN_VALUE;
+            int right1 = (i < m) ? nums1[i] : Integer.MAX_VALUE;
+            int left2 = (j > 0) ? nums2[j - 1] : Integer.MIN_VALUE;
+            int right2 = (j < n) ? nums2[j] : Integer.MAX_VALUE;
+
+            // partition is correct
+            if (left1 <= right2 && left2 <= right1) {
+                // even
+                if (total % 2 == 0) {
+                    result =
+                        (Math.max(left1, left2) + Math.min(right1, right2)) /
+                        2.0;
+                    // odd
+                } else {
+                    result = Math.max(left1, left2);
+                }
+                break;
             }
-            else if(l1>r2) high = cut1-1;
-            else low = cut1+1;
+            // partition is wrong (update left/right pointers)
+            else if (left1 > right2) {
+                right = i - 1;
+            } else {
+                left = i + 1;
+            }
         }
-        return 0.0;
+
+        return result;
     }
 }
