5904c243fca2d013f3be57165c9a6ace12eb008d
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-03 08:09:19 -0400

    Time: 4 ms (20.79%), Space: 44.2 MB (6.59%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -4,15 +4,14 @@ class Solution {
         int maxArea = 0;
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         int[][] directions = new int[][]{{1,0} , {-1,0} ,{0,1} ,{0,-1}};
         
         for(int i = 0 ; i < m ; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
+                if(grid[i][j] == 1){
+                    grid[i][j] = 0;
                     int area = 1;
-                    visited[i][j] = true;
                     q.add(new Pair(i, j));
                     while(!q.isEmpty()){
                         Pair<Integer, Integer> p = q.poll();
@@ -22,9 +21,9 @@ class Solution {
                             int newRow = x + dir[0];
                             int newCol = y + dir[1];
                             if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n){
-                                if(!visited[newRow][newCol] && grid[newRow][newCol] == 1){
+                                if(grid[newRow][newCol] == 1){
                                     q.add(new Pair(newRow, newCol));
-                                    visited[newRow][newCol] = true;
+                                    grid[newRow][newCol] = 0;
                                     area++;
                                 }   
                             }
