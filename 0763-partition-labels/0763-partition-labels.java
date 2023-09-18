82bf8e6d1438a1283e114712f26bd764ada24913
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-18 13:52:16 -0400

    Time: 12 ms (11.90%), Space: 42.1 MB (7.01%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,44 +1,28 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        int n = s.length();
+        if(n == 0) return res;
         HashMap<Character, int[]> indexMap = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            char curr = s.charAt(i);
-            if(indexMap.containsKey(curr)){
-                int[] newIdx = indexMap.get(curr);
-                newIdx[1] = i;
-                indexMap.put(curr, newIdx);
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            if(indexMap.containsKey(c)){
+                indexMap.put(c, new int[]{indexMap.get(c)[0], i});
             } else{
-                int[] newIdx = new int[2];
-                newIdx[0] = i;
-                newIdx[1] = i;
-                indexMap.put(curr, newIdx);
+                indexMap.put(c, new int[]{i, i});
             }
         }
-        // for(Map.Entry<Character, int[]> e : indexMap.entrySet()){
-        //     System.out.println(e.getKey() + " " + Arrays.toString(e.getValue()));
-        // }
+        int end = 0;
+        int prev = -1;
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            end = Math.max(end, indexMap.get(c)[1]);
+            if(end == i){
+                res.add(i - prev);
+                prev = i;
+            }
+        }
+        return res;
         
-        List<Integer> l = new ArrayList<>();
-        int farthest = 0;
-        int i = 0; 
-        while(i < s.length()){
-            int[] curr = indexMap.get(s.charAt(i));
-            farthest = curr[1];
-            int newFarthest = farthest;
-            for(int j = i+1; j < s.length(); j++){
-                if(j >= newFarthest) break;
-                newFarthest = Math.max(newFarthest, indexMap.get(s.charAt(j))[1]);
-            }
-            if(l.size() > 0){
-                l.add(newFarthest+1 - i);
-            }else{
-                 l.add(newFarthest+1);
-            }
-            i = newFarthest+1;
-               
-            
-        }
-        return l;
-
     }
 }
