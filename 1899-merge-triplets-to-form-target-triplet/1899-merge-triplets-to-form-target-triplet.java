0b6de790316f91112a3255cc74cc04faeaa43471
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-18 13:19:18 -0400

    Time: 2 ms (87.78%), Space: 104.9 MB (72.58%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         int[] res = new int[3];
-        for(int[] t : triplets){
+        
+        for (int[] t : triplets) {
             boolean canUpdate = true;
-            for(int i = 0; i < t.length; i++){
-                if(t[i] > target[i]){
+            for (int i = 0; i < 3; i++) {
+                if (t[i] > target[i]) {
                     canUpdate = false;
                     break;
                 }
             }
-            if(canUpdate){
-                for(int i = 0; i < 3; i++){
+            
+            if (canUpdate) {
+                for (int i = 0; i < 3; i++) {
                     res[i] = Math.max(res[i], t[i]);
+                }
+                
+                // Early exit if the result is already equal to the target
+                if (Arrays.equals(res, target)) {
+                    return true;
                 }
             }
         }
-        for(int i = 0; i < 3; i++){
-            if(res[i] != target[i]) return false;
-        }
-        return true;
+        
+        return Arrays.equals(res, target);
     }
 }
