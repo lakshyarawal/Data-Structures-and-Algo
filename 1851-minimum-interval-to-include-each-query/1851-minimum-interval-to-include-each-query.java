290612b08d9edcfd559ea8ae11409b0c3a1de392
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-18 18:07:02 -0400

    Time: 85 ms (86.28%), Space: 103.8 MB (14.96%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,42 +1,23 @@
 class Solution {
-   
-     public int[] minInterval(int[][] intervals, int[] queries) {
-        
-         Arrays.sort(intervals, Comparator.comparingInt(a-> ((int[])a)[0]));
-         int[] copyQueries = queries.clone();
-         Arrays.sort(copyQueries);
-         
-         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> ((int[])a)[0]));
-         Map<Integer, Integer> resMap = new HashMap<>();
-         
-         int N = intervals.length;
-         int i=0;
-         for(int k = 0; k < copyQueries.length; k++){
-             int q = copyQueries[k];
-             if(k>0 && q==copyQueries[k-1])
-                 continue;
-             
-             //add eligible intervals based on start time(i.e start <= q)
-             while(i < N && intervals[i][0] <= q){
-                 pq.offer(new int[]{intervals[i][1] - intervals[i][0] + 1,
-                                    intervals[i][1]});
-                 i++;
-             }
-             
-             //remove ineligible intervals based on end time(i.e end < q)
-             while(!pq.isEmpty() && pq.peek()[1] < q)
-                 pq.poll();
-             
-             resMap.put(q, pq.isEmpty() ? -1 : pq.peek()[0]);
-         }
-         
-         int[] res = new int[queries.length];
-         int ind=0;
-         for(int q : queries){
-             res[ind++] = resMap.get(q);
-         }
-         
-         return res;
-     }
-   
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        HashMap<Integer,Integer> map = new HashMap<>(); 
+        Arrays.sort(intervals,(x,y)->(x[0]-y[0]));
+        int arr[] = new int[queries.length],i=0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)->((x[1]-x[0]) - (y[1]-y[0])));
+        for(int j=0;j<arr.length;j++) arr[j]=queries[j];
+        Arrays.sort(arr);
+        for(int q : arr){
+            while(i<intervals.length && intervals[i][0]<=q){
+                pq.offer(intervals[i++]);
+            }
+            while(pq.size()!=0 && pq.peek()[1]<q){
+                pq.poll();
+            }
+            map.put(q,(pq.size()==0)?-1:pq.peek()[1]-pq.peek()[0]+1);
+        }
+        for(i=0;i<queries.length;i++){
+            queries[i]=map.get(queries[i]);
+        }
+        return queries;
+    }
 }
