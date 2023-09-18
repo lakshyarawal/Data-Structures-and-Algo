b8ecc53d195c08408ca1c37a922517d27edfc35f
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-18 13:54:04 -0400

    Time: 9 ms (40.53%), Space: 41.7 MB (18.12%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -4,19 +4,18 @@ class Solution {
         int n = s.length();
         if(n == 0) return res;
         HashMap<Character, int[]> indexMap = new HashMap<>();
+        char[] ch = s.toCharArray();
         for(int i = 0; i < n; i++){
-            char c = s.charAt(i);
-            if(indexMap.containsKey(c)){
-                indexMap.put(c, new int[]{indexMap.get(c)[0], i});
+            if(indexMap.containsKey(ch[i])){
+                indexMap.put(ch[i], new int[]{indexMap.get(ch[i])[0], i});
             } else{
-                indexMap.put(c, new int[]{i, i});
+                indexMap.put(ch[i], new int[]{i, i});
             }
         }
         int end = 0;
         int prev = -1;
         for(int i = 0; i < n; i++){
-            char c = s.charAt(i);
-            end = Math.max(end, indexMap.get(c)[1]);
+            end = Math.max(end, indexMap.get(ch[i])[1]);
             if(end == i){
                 res.add(i - prev);
                 prev = i;
