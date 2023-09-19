ed2425e21539e4d86b15086e6457d8a90db196d3
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-19 19:37:23 -0400

    Time: 11 ms (78.65%), Space: 44.5 MB (34.95%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -2,16 +2,13 @@ class Solution {
     public int partitionString(String s) {
         int[] counts = new int[26];
         int partitions = 1;
-        // String cs = "";
+
         for(int i = 0; i < s.length(); i++){
             if(counts[s.charAt(i)-'a'] > 0){
-                // System.out.println("IF: "+ cs);
-                // cs = ""+ s.charAt(i);
                 partitions++;
                 counts = new int[26];
                 counts[s.charAt(i)-'a'] = 1;
             }else{
-                // cs += s.charAt(i);
                 counts[s.charAt(i)-'a'] = 1;
             }
         }
