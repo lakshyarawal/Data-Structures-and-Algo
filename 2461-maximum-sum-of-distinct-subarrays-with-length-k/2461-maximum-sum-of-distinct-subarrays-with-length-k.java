f37f2e2bb2a337f543d5c8e0c731eacaeed57fd6
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-09-20 09:30:08 -0400

    Time: 66 ms (9.02%), Space: 56.6 MB (59.41%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,32 +1,21 @@
-class Solution {
-    public long maximumSubarraySum(int[] nums, int k) {
-        long maxSum =0;
-        long curSum =0;
-        int j=0;
-        HashMap<Integer, Integer> map = new HashMap();
-        for(int i=0;i<nums.length;i++){
-            curSum += nums[i];
-            if(map.containsKey(nums[i])){
-                int firstInstance = map.get(nums[i]);
-  
-                for(int m=j;m<=firstInstance;m++){
-                    map.remove(nums[m]);
-                    curSum -=nums[m];
-                }
-                j=firstInstance+1;
-            }
+class Solution 
+{
+    public long maximumSubarraySum(int[] nums, int k) 
+    {
+        Long res = 0L, cur = 0L, dup = -1L;
+        int[] pos = IntStream.range(0, 100001).map(j -> -1).toArray();
+        
+        for (int i = 0; i < nums.length; ++i)
+        {
+            cur += nums[i];
+            if (i >= k) cur -= nums[i-k];
             
-            map.put(nums[i], i);
-
-            if(map.size() == k){
-                maxSum = Math.max(maxSum,curSum);
-                // each time size of map == k , remove the first element
-                int firstElement = nums[i-k+1];
-                curSum -= firstElement;
-                map.remove(firstElement);
-                j++; // increment window start index
-            }             
-        }   
-        return maxSum;
+            dup = Math.max(dup, pos[nums[i]]);
+            if (i - dup >= k) res = Math.max(res, cur);
+            
+            pos[nums[i]] = i;
+        }
+        
+        return res;
     }
 }
