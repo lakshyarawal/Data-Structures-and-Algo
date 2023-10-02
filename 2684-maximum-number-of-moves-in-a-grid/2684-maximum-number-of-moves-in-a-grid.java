99b3d281fc58995b1f3b3465b59570949391b6b2
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-10-02 12:52:02 -0400

    Time: 5 ms (92.43%), Space: 55.4 MB (12.97%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -1,23 +1,19 @@
 class Solution {
-    int dirs[][] = {{0, 1}, {1, 1}, {-1, 1}}, m, n, cache[][];
     public int maxMoves(int[][] grid) {
-        m = grid.length; n = grid[0].length; cache = new int[m][n];
-        Arrays.stream(cache).forEach(row -> Arrays.fill(row, -1));
-
-        int res = 0;
-        for (int i = 0; i < m; i++)
-            res = Math.max(res, dp(grid, i, 0));
-        return res;
-    }
-    
-    private int dp(int[][] grid, int i, int j) {
-        if (cache[i][j] != -1) return cache[i][j];
-        int ans = 0;
-        for (int[] dir : dirs) {
-            int ni = i + dir[0], nj = j + dir[1];
-            if (ni >= 0 && ni < m && nj < n && grid[i][j] < grid[ni][nj])
-                ans = Math.max(ans, 1 + dp(grid, ni, nj));
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            cnt=Math.max(cnt,check(i,0,grid,0));
         }
-        return cache[i][j] = ans;
+        return cnt-1;
+    }
+    public static int  check(int i,int j,int grid[][],int prev){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]<=prev) return 0;
+        int prev1=grid[i][j];
+        grid[i][j]=(int)1e9;
+        int up=check(i-1,j+1,grid,prev1);
+        int nxt=check(i,j+1,grid,prev1);
+        int down=check(i+1,j+1,grid,prev1);
+        // System.out.println(up+" "+nxt+" "+down);
+        return Math.max(up,Math.max(nxt,down))+1;
     }
 }
