7e0c709686df8051b63e726299405e04cfb707ad
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-10-24 14:22:54 -0600

    Time: 1 ms (99.73%), Space: 44.1 MB (8.75%) - LeetHub

--- 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.java
+++ 2903-find-indices-with-index-and-value-difference-i/2903-find-indices-with-index-and-value-difference-i.java
@@ -1,12 +1,41 @@
 class Solution {
-    public int[] findIndices(int[] A, int d, int v) {
-        int mini = 0, maxi = 0, n = A.length;
-        for (int i = d; i < n; i++) {
-            if (A[i - d] < A[mini]) mini = i - d;
-            if (A[i - d] > A[maxi]) maxi = i - d;
-            if (A[i] - A[mini] >= v) return new int[] {mini, i};
-            if (A[maxi] - A[i] >= v) return new int[] {maxi, i};
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int[] ans = new int[]{-1,-1};
+        
+        List<int[]> list = new ArrayList<>();
+        int min = 0;
+        int max = 0;
+        
+        
+        //store the max and min till the index i
+        for(int i =0;i < nums.length; ++i){
+            
+            if(nums[i] < nums[min]) min = i;
+            if(nums[i] > nums[max]) max = i;
+            list.add(new int[]{min, max});
         }
-        return new int[] { -1, -1};
+        
+        //try to find the second index where the condition can be satisfied
+        for(int i =0; i < nums.length; ++i){
+            
+            int j = i-indexDifference;
+            
+            if(j>=0){
+                
+               if(Math.abs(nums[i] - nums[list.get(j)[0]])>=valueDifference){
+                   return new int[]{list.get(j)[0], i};
+               } 
+                
+               if(Math.abs(nums[i] - nums[list.get(j)[1]])>=valueDifference){
+                   return new int[]{list.get(j)[1], i};
+               }  
+                    
+            }
+            
+        }
+        
+
+        // We can't satisfy the condition
+        return new int[]{-1,-1};
     }
 }
