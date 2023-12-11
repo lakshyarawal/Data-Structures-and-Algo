3971e0047f62cf468c2a82191cf89c6d3ae85f04
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-12-10 23:20:57 -0500

    Time: 116 ms (83.81%), Space: 16.3 MB (71.80%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,8 @@
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        for l in mat:
+            n = len(l)
+            for i in range(n):
+                if l[i] != l[(i + k) % n]:
+                    return False
+        return True
