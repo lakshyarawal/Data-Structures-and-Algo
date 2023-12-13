d043c96f30a90d18cc2ce3791a1309f35c158236
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-12-13 16:43:28 -0500

    Time: 1 ms (100.00%), Space: 43.7 MB (81.37%) - LeetHub

--- 2937-make-three-strings-equal/2937-make-three-strings-equal.java
+++ 2937-make-three-strings-equal/2937-make-three-strings-equal.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findMinimumOperations(String s1, String s2, String s3) {
         int i = 0;
-        
-        int minLength = Math.min(s1.length(), Math.min(s2.length(), s3.length())); 
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+        int minLength = Math.min(l1, Math.min(l2, l3)); 
         for(;i < minLength; i++){
             if(s1.charAt(i) == s2.charAt(i) && s2.charAt(i) == s3.charAt(i)) continue;
             else break;
         }
-        return i==0 ? -1 : (s1.length() + s2.length() + s3.length() - (i*3));
+        return i==0 ? -1 : (l1 + l2 + l3 - (i*3));
     }
 }
