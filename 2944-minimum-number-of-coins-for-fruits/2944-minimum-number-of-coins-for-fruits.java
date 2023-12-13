fb7bb2fc49badb688bea83fc914da9ceed13d82c
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-12-13 16:22:41 -0500

    Time: 39 ms (33.58%), Space: 56.2 MB (14.53%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.java
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.java
@@ -1,15 +1,28 @@
 class Solution {
-    int[] dp = new int[1001];
-    int solve(int[] prices, int i){
-        if(i >= prices.length) return 0;
-        if(dp[i] != 0) return dp[i];
-        int res = Integer.MAX_VALUE;
-        for(int j = i+1; j <= 2*i + 2; ++j) {
-            res = Math.min(res, solve(prices, j));
-        }
-        return dp[i] = res + prices[i];
-    }
     public int minimumCoins(int[] prices) {
-        return solve(prices, 0);
+        
+        int length = prices.length;
+        Integer[][] dp = new Integer[length][length+1];
+        int ans = function(0,0,prices,dp);
+        return ans;
+        
     }
+    
+    public int function(int ind,int free_fruits,int[] prices,Integer[][] dp){
+        
+        if(ind==prices.length) return 0;
+        
+        if(dp[ind][free_fruits]!=null) return dp[ind][free_fruits];
+        
+        int take_fruit = Integer.MAX_VALUE;
+        if(free_fruits>=1){
+            take_fruit = Math.min(prices[ind]+function(ind+1,ind+1,prices,dp),0+function(ind+1,free_fruits-1,prices,dp));
+        }else{
+            take_fruit = prices[ind]+function(ind+1,ind+1,prices,dp);
+        }
+        
+        return dp[ind][free_fruits]=take_fruit;
+        
+    }
+    
 }
