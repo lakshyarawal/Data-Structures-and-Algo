6ec8a8805ebd599a7d2094d3c7aa227d6fb8b237
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2023-12-27 21:38:41 -0800

    Time: 106 ms (5.41%), Space: 17.6 MB (7.15%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,7 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        new_nums = [] # create an empty list
-        for i in range(len(nums)//2): # loop through only half of the list
-            new_nums.append(nums[i]) # append the first element
-            new_nums.append(nums[i+n]) # append the [i+n]'th element
-        return new_nums # returned the filled list
+        stack = list()
+        j = n
+        i = 0
+        while i < n:
+            stack.append(nums[i])
+            stack.append(nums[j])
+            j+=1
+            i+=1
+        return stack
