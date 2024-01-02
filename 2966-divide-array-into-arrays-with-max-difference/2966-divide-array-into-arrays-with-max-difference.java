65057d93e4af900bc8b925ed4086340efde4fab1
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-01-02 13:21:08 -0800

    Time: 46 ms (6.16%), Space: 58.4 MB (21.28%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -1,36 +1,25 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-        int len = nums.length;
-        int[][] ans = new int[len/3][3];
-        
-        int r = -1, c = 0, first=0;
-        
-        int max = 0;
-        for(int i : nums) {
-            if(i>max) max = i;
-        }     
-         
-        int freq[] = new int[max+1];
-        for(int i : nums) {
-            freq[i]++;
-        }      
-        
-        
-        for(int i=1; i<max+1;) {
-            if(freq[i] == 0) i++;
-            else{
-                if(c == 0) {
-                    r++;
-                    first = i;
-                }
-                if((i-first) >k) return new int[0][0];
-                ans[r][c] = i;
-                c=(c+1)%3;
-                freq[i]--;
-                
+        int ans[][]=new int[nums.length/3][3];
+        int ind=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i+=3)
+        {
+            int first=nums[i];
+            int second=nums[i+1];
+            int third=nums[i+2];
+            if(third-first <= k)
+            {
+                ans[ind][0]=first;
+                ans[ind][1]=second;
+                ans[ind][2]=third;
+                ind++;
+            }
+            else
+            {
+                return new int[0][0];
             }
         }
-        
-        return ans;
+        return ans; 
     }
 }
