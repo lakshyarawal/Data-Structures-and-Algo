44a8485219fc820256db83014aa635b7d9033018
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-01-29 22:39:14 -0800

    Time: 145 ms (82.39%), Space: 17.8 MB (21.09%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,26 +1,15 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        n = len(word)
-        if(n < 9):
-            return n
-        # Create a dictionary finding the frequecy of each alphabet in the word
-        # Sorting this based on frequency
-        # For the first 8 assign 1 value, next 8 2 and the next 8 3 and rest 4
-        # multiply these values
-        character_frequencies = {}
-        for character in word:
-            if character in character_frequencies.keys():
-                character_frequencies[character] += 1
-            else:
-                character_frequencies[character] = 1
-        sorted_dict_desc = dict(sorted(character_frequencies.items(), key=lambda item: item[1], reverse=True))
-        res = 0
-        counter = 0
-        multiplier = 1
-        for key, value in sorted_dict_desc.items():
-            res += value * multiplier
-            counter += 1
-            if counter % 8 == 0:
-                multiplier += 1
-        #print(sorted_dict_desc)
-        return res
+        m = Counter(word)
+        v = sorted(m.values(), reverse=True)
+        ans = 0
+        for i in range(1, len(v) + 1):
+            if i <= 8:
+                ans += v[i - 1]
+            if 8 < i <= 16:
+                ans += v[i - 1] * 2
+            if 16 < i <= 24:
+                ans += v[i - 1] * 3
+            if i > 24:
+                ans += v[i - 1] * 4
+        return ans
