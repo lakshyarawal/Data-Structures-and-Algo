a26b6cbe4cf90f46cc060ecfb2ba749415fa2b1a
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-01-29 23:44:47 -0800

    Time: 52 ms (65.52%), Space: 16.4 MB (76.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,49 +1,65 @@
 class Solution:
-        
-    def dfs(self, r, c, grid, t, time):
-        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        for dir_ in dirs:
-            newr, newc = r + dir_[0], c + dir_[1]
-            if (
-                newr >= len(grid)
-                or newr < 0
-                or newc >= len(grid[r])
-                or newc < 0
-            ):
-                continue
-            if (
-                grid[newr][newc] == 1
-                or (grid[newr][newc] == -1 and time[newr][newc] > t + 1)
-            ):
-                grid[newr][newc] = -1
-                time[newr][newc] = min(time[newr][newc], t + 1)
-                self.dfs(newr, newc, grid, t + 1, time)
-                
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n, res = len(grid), len(grid[0]), 0
-        if m == 0 or n == 0:
-            return 0
-
-        time = [[0] * n for _ in range(m)]
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 2:
-                    time[i][j] = 0
-                if grid[i][j] == 1:
-                    time[i][j] = float('inf')
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 2:
-                    self.dfs(i, j, grid, 0, time)
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    return -1
-                elif grid[i][j] == -1:
-                    res = max(res, time[i][j])
-
-        return res
         
+        # number of rows
+        rows = len(grid)
+        if rows == 0:  # check if grid is empty
+            return -1
+        
+        # number of columns
+        cols = len(grid[0])
+        
+        # keep track of fresh oranges
+        fresh_cnt = 0
+        
+        # queue with rotten oranges (for BFS)
+        rotten = deque()
+        
+        # visit each cell in the grid
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    # add the rotten orange coordinates to the queue
+                    rotten.append((r, c))
+                elif grid[r][c] == 1:
+                    # update fresh oranges count
+                    fresh_cnt += 1
+        
+        # keep track of minutes passed.
+        minutes_passed = 0
+        
+        # If there are rotten oranges in the queue and there are still fresh oranges in the grid keep looping
+        while rotten and fresh_cnt > 0:
+
+            # update the number of minutes passed
+            # it is safe to update the minutes by 1, since we visit oranges level by level in BFS traversal.
+            minutes_passed += 1
+            
+            # process rotten oranges on the current level
+            for _ in range(len(rotten)):
+                x, y = rotten.popleft()
+                
+                # visit all the adjacent cells
+                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    # calculate the coordinates of the adjacent cell
+                    xx, yy = x + dx, y + dy
+                    # ignore the cell if it is out of the grid boundary
+                    if xx < 0 or xx == rows or yy < 0 or yy == cols:
+                        continue
+                    # ignore the cell if it is empty '0' or visited before '2'
+                    if grid[xx][yy] == 0 or grid[xx][yy] == 2:
+                        continue
+                        
+                    # update the fresh oranges count
+                    fresh_cnt -= 1
+                    
+                    # mark the current fresh orange as rotten
+                    grid[xx][yy] = 2
+                    
+                    # add the current rotten to the queue
+                    rotten.append((xx, yy))
+
+        
+        # return the number of minutes taken to make all the fresh oranges to be rotten
+        # return -1 if there are fresh oranges left in the grid (there were no adjacent rotten oranges to make them rotten)
+        return minutes_passed if fresh_cnt == 0 else -1
