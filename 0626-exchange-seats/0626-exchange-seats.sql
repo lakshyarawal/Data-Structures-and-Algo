fc1bed64a3622f8843b8934efa7571f5b118077a
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-02-10 19:06:09 -0800

    Time: 1020 ms (10.01%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,22 +1,5 @@
-# Write your MySQL query statement below
-with all_students as(
-    SELECT
-        LEAD(student) OVER (ORDER BY id ASC) AS next_student,
-        LAG(student) OVER (ORDER BY id ASC) AS prev_student,
-        student,
-        id
-    FROM
-        Seat
-)
-SELECT
-    id,
-    (CASE
-        WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM Seat) THEN student
-        WHEN id % 2 = 1 THEN next_student
-        ELSE prev_student
-     END) as student
-FROM 
-    all_students
-    
-    
-
+SELECT id, (CASE
+WHEN id % 2 = 1 THEN COALESCE(LEAD(student, 1) OVER (ORDER BY id ASC), student)
+ELSE LAG(student, 1) OVER (ORDER BY id ASC)
+END) as student
+FROM Seat
