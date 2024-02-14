a2b18d207ba30bfcf64749db06a2fc4ad6a53da1
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-02-14 12:45:54 -0800

    Time: 376 ms (98.93%), Space: 30.5 MB (17.43%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,17 +1,12 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        skill.sort()
-        start = 0
-        end = len(skill) -1
-        total = skill[start] + skill[end]
-        res = skill[start] * skill[end]
-        start += 1
-        end -= 1
-        while start < end:
-            if skill[start] + skill[end] != total:
-                return -1
-            res += skill[start] * skill[end]
-            start += 1
-            end -= 1
+        skill_est = 2 * sum(skill) // len(skill)        # [1] compute each team's expected skill
+        chemistry = 0     
+        cnt_values = Counter(skill)                 # [2] count skills
+
+        for key, val in cnt_values.items():          # [3] for all players with a given skill, 
+            if val != cnt_values[skill_est-key]:             #     check that there is the same number
+                return -1                #     of players with paired skill...
+            chemistry += val * key * (skill_est-key)    # [4] ...and compute chemistry for these pairs
             
-        return res
+        return chemistry // 2             # [5] we double-counted, correct for this
