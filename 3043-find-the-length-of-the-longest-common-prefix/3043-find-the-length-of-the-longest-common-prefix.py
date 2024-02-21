65fa651352d75af67688df67bc0280caa701ff76
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-02-20 23:25:19 -0800

    Time: 1129 ms (18.92%), Space: 35.6 MB (15.28%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,18 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.end = False
+        self.child = [None] * 10
+
 class Solution:
-    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        pSet = set()
+    def __init__(self):
+        self.root = TrieNode()
+
+    def longestCommonPrefix(self, arr1, arr2):
+        if len(arr2) > len(arr1):
+            return self.longestCommonPrefix(arr2, arr1)
         for num in arr1:
-            while num > 0:
-                pSet.add(num)
-                num //= 10
+            self.add(num)
         max_len = 0
         for num in arr2:
-            while num > 0:
-                n = len(str(num))
-                if n <= max_len:  # if the num itself is less than max no point in checking its prefix
-                    break
-                if num in pSet:
-                    max_len = n
-                    break  # no point in checking smaller prefix as they would be less than max anyway
-                num //= 10
+            max_len = max(max_len, self.prefixCount(num))
         return max_len
+
+    def add(self, num):
+        cur = self.root
+        for ch in str(num):
+            index = int(ch)
+            if cur.child[index] is None:
+                cur.child[index] = TrieNode()
+            cur = cur.child[index]
+        cur.end = True
+
+    def prefixCount(self, num):
+        cur = self.root
+        count = 0
+        for ch in str(num):
+            index = int(ch)
+            if cur.child[index] is None:
+                return count
+            cur = cur.child[index]
+            count += 1
+        return count
