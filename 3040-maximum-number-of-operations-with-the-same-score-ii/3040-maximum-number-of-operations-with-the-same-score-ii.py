e65e70c78ac9a8c7c5bcf944fa7ffac1c59b230c
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-02-22 19:43:39 -0800

    Time: 1941 ms (55.91%), Space: 361.6 MB (48.50%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,29 +1,21 @@
 class Solution:
     def maxOperations(self, nums):
-        if len(nums) == 2:
-            return 1
         n = len(nums)
-        ans = self.solve(nums, n, nums[0] + nums[1])
-        ans = max(ans, self.solve(nums, n, nums[0] + nums[n - 1]))
-        ans = max(ans, self.solve(nums, n, nums[n - 1] + nums[n - 2]))
-        return ans
-    
-    def solve(self, nums, n, target):
-        dp = [[-1] * n for _ in range(n)]
-        ans = self.helper(nums, 0, n - 1, target, dp)
-        return ans
-    
-    def helper(self, nums, l, r, target, dp):
-        if l >= r:
-            return 0
-        if dp[l][r] != -1:
-            return dp[l][r]
-        ans = 0
-        if nums[l] + nums[r] == target:
-            ans = max(ans, 1 + self.helper(nums, l + 1, r - 1, target, dp))
-        if l + 1 <= r and nums[l] + nums[l + 1] == target:
-            ans = max(ans, 1 + self.helper(nums, l + 2, r, target, dp))
-        if l <= r - 1 and nums[r] + nums[r - 1] == target:
-            ans = max(ans, 1 + self.helper(nums, l, r - 2, target, dp))
-        dp[l][r] = ans
-        return ans
+
+        @lru_cache(None)
+        def dfs(l,r,score):
+            if l >= n or r <= 0 or l >= r:
+                return 0 
+
+            max_count = 0
+
+            if nums[l] + nums[r] == score:
+                max_count = max(max_count,1+dfs(l+1,r-1,score))
+            if l+1 <= r and nums[l] + nums[l+1] == score:
+                max_count = max(max_count,1+dfs(l+2,r,score))
+            if r-1 >= l and nums[r] + nums[r-1] == score:
+                max_count = max(max_count,1+dfs(l,r-2,score))
+
+            return max_count
+
+        return 1 + max(dfs(2,n-1,nums[0]+nums[1]),dfs(1,n-2,nums[0]+nums[-1]),dfs(0,n-3,nums[-1]+nums[-2]))
