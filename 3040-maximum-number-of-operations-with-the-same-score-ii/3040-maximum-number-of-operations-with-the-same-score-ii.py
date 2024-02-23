0a6c4bf8e9f07371d134f6ae7c56c3f9b29fdc10
Lakshya Rawal <lakshyarawal.lr@gmail.com>
2024-02-22 19:36:10 -0800

    Time: 4292 ms (10.70%), Space: 65.7 MB (89.74%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.py
@@ -1,12 +1,29 @@
 class Solution:
+    def maxOperations(self, nums):
+        if len(nums) == 2:
+            return 1
+        n = len(nums)
+        ans = self.solve(nums, n, nums[0] + nums[1])
+        ans = max(ans, self.solve(nums, n, nums[0] + nums[n - 1]))
+        ans = max(ans, self.solve(nums, n, nums[n - 1] + nums[n - 2]))
+        return ans
     
-    def maxOperations(self, A: List[int]) -> int:
-        @cache
-        def go(i = 0, j = len(A) - 1, last = 0):
-            if j <= i:
-                return 0
-            a = 1 + go(i + 2, j, A[i] + A[i + 1]) if not last or last == A[i] + A[i + 1] else 0
-            b = 1 + go(i, j - 2, A[j - 1] + A[j]) if not last or last == A[j - 1] + A[j] else 0
-            c = 1 + go(i + 1, j - 1, A[i] + A[j]) if not last or last == A[i] + A[j] else 0
-            return max(a, b, c)
-        return go()
+    def solve(self, nums, n, target):
+        dp = [[-1] * n for _ in range(n)]
+        ans = self.helper(nums, 0, n - 1, target, dp)
+        return ans
+    
+    def helper(self, nums, l, r, target, dp):
+        if l >= r:
+            return 0
+        if dp[l][r] != -1:
+            return dp[l][r]
+        ans = 0
+        if nums[l] + nums[r] == target:
+            ans = max(ans, 1 + self.helper(nums, l + 1, r - 1, target, dp))
+        if l + 1 <= r and nums[l] + nums[l + 1] == target:
+            ans = max(ans, 1 + self.helper(nums, l + 2, r, target, dp))
+        if l <= r - 1 and nums[r] + nums[r - 1] == target:
+            ans = max(ans, 1 + self.helper(nums, l, r - 2, target, dp))
+        dp[l][r] = ans
+        return ans
